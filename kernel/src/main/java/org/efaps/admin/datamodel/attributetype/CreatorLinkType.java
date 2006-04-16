5475514520ba144d8f238ce6bf1c14c945dc4913
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-04-16 11:24:17 +0000

    - added svn properties - header updated to new version
    git-svn-id: https://efaps.googlecode.com/svn/trunk@115 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel/src/main/java/org/efaps/admin/datamodel/attributetype/CreatorLinkType.java
+++ kernel/src/main/java/org/efaps/admin/datamodel/attributetype/CreatorLinkType.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
  */
 
 package org.efaps.admin.datamodel.attributetype;
