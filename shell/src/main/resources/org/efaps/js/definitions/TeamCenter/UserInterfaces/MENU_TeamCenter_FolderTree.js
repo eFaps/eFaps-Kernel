44f017a04a942dcbb91b5eb78ff75f9e4f0c91a5
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-04-24 17:18:33 +0000

    - add history command - update header - add svn properties
    git-svn-id: https://efaps.googlecode.com/svn/trunk@169 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- shell/src/main/resources/org/efaps/js/definitions/TeamCenter/UserInterfaces/MENU_TeamCenter_FolderTree.js
+++ shell/src/main/resources/org/efaps/js/definitions/TeamCenter/UserInterfaces/MENU_TeamCenter_FolderTree.js
@@ -1,21 +1,36 @@
 /*******************************************************************************
-* Admin_UI_Menu:
-* ~~~~~~~~~~~~~~
-* TeamCenter_FolderTree
-*
-* Description:
-* ~~~~~~~~~~~~
-*
-* History:
-* ~~~~~~~~
-* Revision: $Rev$
-* Date:     $Date$
-* By:       $Author$
-*
-* Author:
-* ~~~~~~~
-* TMO
-*******************************************************************************/
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************
+ * Admin_UI_Menu:
+ * ~~~~~~~~~~~~~~
+ * TeamCenter_FolderTree
+ *
+ * Description:
+ * ~~~~~~~~~~~~
+ *
+ *
+ * History:
+ * ~~~~~~~~
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ *
+ * Author:
+ * ~~~~~~~
+ * tmo
+ ******************************************************************************/
 
 with (MENU)  {
   addProperty("Target",                       "content");
@@ -24,5 +39,5 @@ with (MENU)  {
   addTargetMenu("TeamCenter_FolderTree_Menu");
   addChild(new Command("TeamCenter_FolderTree_SubFolders"));
   addChild(new Command("TeamCenter_FolderTree_Documents"));
-//  addChild(new Command("Admin_UI_DirectTree_Used"));
+  addChild(new Command("Main_Util_History"));
 }
