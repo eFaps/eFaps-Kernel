6a7cbe10a8e55dd8f9f24023454f0e952a3b5d47
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-06-06 00:46:33 +0000

    - using JAAS also for web authentification
    git-svn-id: https://efaps.googlecode.com/svn/trunk@232 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- webapp/src/main/java/org/efaps/servlet/LoginServlet.java
+++ webapp/src/main/java/org/efaps/servlet/LoginServlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 The eFaps Team
+ * Copyright 2006 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,22 +23,51 @@ package org.efaps.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.TextOutputCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.efaps.admin.user.Person;
 import org.efaps.db.Context;
 
 /**
- * The servlet logs in a user with name and password.
+ * The servlet logs in a user with name and password. The name and password is
+ * checked with the help of Java Authentication and Authorization Service
+ * (short JAAS).
  *
  * @author tmo
- * @version $Rev$
+ * @version $Id$
  */
 public class LoginServlet extends HttpServlet  {
+
+  /**
+   * Logging instance used to give logging information of this class.
+   */
+  private static Log LOG = LogFactory.getLog(LoginServlet.class);
+
+  /**
+   * Name of the servlet initialise parameter for the application.
+   */
+  final private static String INIT_PARAM_APPLICATION = "application";
+
+  /**
+   * Name of the servlet initialise parameter for the application.
+   */
+  final private static String INIT_PARAM_FORWARD_URL = "forwardURL";
 
   /**
    * name of the name parameter
@@ -49,6 +78,48 @@ public class LoginServlet extends HttpServlet  {
    * name of the password parameter
    */
   final private static String PARAM_PASSWORD =        "password";
+
+  /**
+   * The name of the application used to create a new login context.
+   *
+   * @see #checkLogin
+   * @see #init
+   */
+  private String application = "eFaps";
+
+  /**
+   * The forward URL used if the login is correct and the next page must be
+   * shown.
+   *
+   * @see #init
+   * @see #doGet
+   */
+  private String forwardURL = "${COMMONURL}/Main.jsf";
+
+  /**
+   * The login servlet is initialised. The application name in
+   * {@link #application} and the forward URL after login in
+   * {@link #forwardURL} is set.
+   *
+   * @param _config
+   * @see #INIT_PARAM_APPLICATION
+   * @see #application
+   * @see #INIT_PARAM_FORWARD_URL
+   * @see #forwardURL
+   */
+  public void init(final ServletConfig _config) throws ServletException  {
+    super.init(_config);
+
+    String applInit = _config.getInitParameter(INIT_PARAM_APPLICATION);
+    if (applInit != null)  {
+      this.application = applInit;
+    }
+
+    String forwInit = _config.getInitParameter(INIT_PARAM_FORWARD_URL);
+    if (forwInit != null)  {
+      this.forwardURL = forwInit;
+    }
+  }
 
   /**
    * User wants to login into eFaps. The user name and password is checked.
@@ -73,19 +144,20 @@ public class LoginServlet extends HttpServlet  {
 
       _res.setContentType("text/html");
 
-String newUrl = (String) _req.getSession().getAttribute(SecurityFilter.SESSIONPARAM_LOGIN_FORWARD);
-if (newUrl == null)  {
-  newUrl = RequestHandler.replaceMacrosInUrl("${COMMONURL}/Main.jsf");
-} else  {
-  _req.getSession().removeAttribute(SecurityFilter.SESSIONPARAM_LOGIN_FORWARD);
-}
-_res.sendRedirect(newUrl);
+      String newUrl = (String) _req.getSession().getAttribute(SecurityFilter.SESSIONPARAM_LOGIN_FORWARD);
+      if (newUrl == null)  {
+        newUrl = RequestHandler.replaceMacrosInUrl(this.forwardURL);
+      } else  {
+        _req.getSession().removeAttribute(SecurityFilter.SESSIONPARAM_LOGIN_FORWARD);
+      }
+      _res.sendRedirect(newUrl);
     } else  {
       doSendLoginFrameNotCorrect(_req, _res);
     }
   }
 
   /**
+   * This page is sent if the login is not correct.
    *
    * @param _req request variable
    * @param _res response variable
@@ -112,35 +184,86 @@ _res.sendRedirect(newUrl);
     } catch(IOException e)  {
       throw e;
     } catch (Exception e)  {
-e.printStackTrace();
-throw new ServletException(e);
+      LOG.error("Could not write the frame for not correct login.", e);
+      throw new ServletException(e);
     } finally  {
       pW.close();
     }
   }
 
   /**
-   * The instance method checks if for the given user the password is correct
-   * and the person is active (status equals 10001).
+   * The instance method checks if for the given user the password is correct.
+   * The test itself is done with
    *
    * @param _name   name of the person name to check
    * @param _passwd password of the person to check
    * @see #checkLogin
    */
-  protected boolean checkLogin(String _name, String _passwd)  {
+  protected boolean checkLogin(final String _name, final String _passwd)  {
     boolean ret = false;
-    Context context = null;
     try  {
-      context = new Context();
-      if (_name!=null)  {
-        Person person = Person.get(_name);
-        ret = person.checkPassword(context, _passwd);
-      }
-    } catch (Throwable e)  {
-e.printStackTrace();
-    } finally  {
-      context.close();
+      LoginContext login = new LoginContext(this.application,
+              new LoginCallBackHandler(_name, _passwd));
+      login.login();
+      ret = true;
+    } catch (LoginException e)  {
+      LOG.error("login failed for '" + _name + "'", e);
     }
     return ret;
+  }
+
+
+  private class LoginCallBackHandler implements CallbackHandler  {
+
+    /**
+     * The user name to test is stored in this instance variable.
+     */
+    private final String name;
+
+    /**
+     * The password used from the user is stored in this instance variable.
+     */
+    private final String password;
+
+    /**
+     * Constructor initialising the name and password in this callback
+     * handler.
+     *
+     * @see #name
+     * @see #password
+     */
+    private LoginCallBackHandler(final String _name, final String _passwd)  {
+      this.name = _name;
+      this.password = _passwd;
+    }
+
+    /**
+     * The handler sets for instances of {@link NameCallBack} the given
+     * {@link #name} and for instances of {@link PasswordCallBack} the given
+     * {@link #password}. {@link TextOutputCallBack} instances are ignored.
+     *
+     * @param _callbacks callback instances to handle
+     * @throws UnsupportedCallbackException for all {@link Callback}
+     *         instances which are not {@link NameCallBack},
+     *         {@link PasswordCallBack} or {@link TextOutputCallBack}.
+     */
+    public void handle(final Callback[] _callbacks)
+      throws IOException, UnsupportedCallbackException {
+
+      for (int i = 0; i < _callbacks.length; i++) {
+        if (_callbacks[i] instanceof TextOutputCallback) {
+          // do nothing, TextOutputCallBack's are ignored!
+        } else if (_callbacks[i] instanceof NameCallback) {
+          NameCallback nc = (NameCallback) _callbacks[i];
+          nc.setName(this.name);
+        } else if (_callbacks[i] instanceof PasswordCallback) {
+          PasswordCallback pc = (PasswordCallback)_callbacks[i];
+          pc.setPassword(this.password.toCharArray());
+        } else {
+          throw new UnsupportedCallbackException
+              (_callbacks[i], "Unrecognized Callback");
+        }
+      }
+    }
   }
 }
