443a33438c5fccaf53fb253f9bed4b88cae41678
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-07-15 22:37:05 +0000

    - groupings are not searchable - svn properties added - header updated
    git-svn-id: https://efaps.googlecode.com/svn/trunk@240 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- shell/src/main/resources/org/efaps/js/definitions/Admin/User/UserInterfaces/FORM_Admin_User_PersonForm.js
+++ shell/src/main/resources/org/efaps/js/definitions/Admin/User/UserInterfaces/FORM_Admin_User_PersonForm.js
@@ -1,21 +1,23 @@
-/*******************************************************************************
-* Admin_UI_Form:
-* ~~~~~~~~~~~~~~
-* Admin_User_PersonForm
-*
-* Description:
-* ~~~~~~~~~~~~
-*
-* History:
-* ~~~~~~~~
-* Revision: $Rev$
-* Date:     $Date$
-* By:       $Author$
-*
-* Author:
-* ~~~~~~~
-* TMO
-*******************************************************************************/
+/*
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author:          tmo
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
 
 with (FORM)  {
   with (addField("type"))  {
@@ -35,6 +37,7 @@ with (FORM)  {
   }
   with (addField("createGroup"))  {
     addProperty("Creatable",              "false");
+    addProperty("Searchable",             "false");
     addProperty("GroupCount",             "2");
   }
   with (addField("creator"))  {
@@ -54,6 +57,7 @@ with (FORM)  {
   }
   with (addField("modifyGroup"))  {
     addProperty("Creatable",              "false");
+    addProperty("Searchable",             "false");
     addProperty("GroupCount",             "2");
   }
   with (addField("modifier"))  {
