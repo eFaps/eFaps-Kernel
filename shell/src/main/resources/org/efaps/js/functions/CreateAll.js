161a19910126ed551f5cdfa93baca79706562b79
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-07-20 14:02:11 +0000

    - remove create of tables of TeamCenter from eFaps and move this   in the teamcenter sub project
    git-svn-id: https://efaps.googlecode.com/svn/trunk@273 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- shell/src/main/resources/org/efaps/js/functions/CreateAll.js
+++ shell/src/main/resources/org/efaps/js/functions/CreateAll.js
@@ -513,40 +513,6 @@ function _eFapsCreateUserTablesStep2(_con, _stmt)  {
 }
 
 /**
- * The private function creates all Team Center tables.
- *
- * @param _stmt SQL statement to work on
- */
-function _eFapsCreateAllTeamCenterTables(_con, _stmt)  {
-
-  print("Create TeamCenter Tables");
-
-  eFapsCommonSQLTableCreate(_con, _stmt, "Folder object", "TCFOLDER", "ABSTRACT",[
-      ["PARENTFOLDER          "+TYPE_INTEGER],
-      ["constraint TCFOLDER_FK_PRNTFOL foreign key(PARENTFOLDER) references TCFOLDER(ID)"]
-  ]);
-
-  eFapsCommonSQLTableCreate(_con, _stmt, "Document object", "TCDOCUMENT", "ABSTRACT",[
-      ["FILENAME              "+TYPE_STRING_SHORT+"(128)"],
-      ["FILELENGTH            "+TYPE_INTEGER]
-  ]);
-
-  eFapsCommonSQLTableCreate(_con, _stmt, "Connection beetween Documents and Folders", "TCDOC2FOL", null,[
-      ["TCDOCUMENT            "+TYPE_INTEGER+"                   not null"],
-      ["TCFOLDER              "+TYPE_INTEGER+"                   not null"],
-      ["CREATOR               "+TYPE_INTEGER+"                   not null"],
-      ["CREATED               "+TYPE_DATETIME+"                  not null"],
-      ["MODIFIER              "+TYPE_INTEGER+"                   not null"],
-      ["MODIFIED              "+TYPE_DATETIME+"                  not null"],
-      ["constraint TCDOC2FOL_UK_FOLDOC unique(TCDOCUMENT,TCFOLDER)"],
-      ["constraint TCDOC2FOL_FK_TCDOC  foreign key(TCDOCUMENT)   references ABSTRACT(ID)"],
-      ["constraint TCDOC2FOL_FK_TCFLDR foreign key(TCFOLDER)     references TCFOLDER(ID)"],
-      ["constraint TCDOC2FOL_FK_CRTR   foreign key(CREATOR)      references USERPERSON(ID)"],
-      ["constraint TCDOC2FOL_FK_MDFR   foreign key(MODIFIER)     references USERPERSON(ID)"]
-  ]);
-}
-
-/**
  * The private functions creates all data model tables
  *
  * @param _stmt SQL statement to work on
@@ -1010,10 +976,8 @@ function createAll()  {
       eFapsCommonVersionInsert(con, stmt, "eFaps", 1);
     }
 
-    if (eFapsCommonVersionGet(con,stmt) < 2)  {
-print("Install VERSION 2 ---------------------");
-    }
-_eFapsCreateAllTeamCenterTables(con, stmt);
+//    if (eFapsCommonVersionGet(con,stmt) < 2)  {
+//    }
 
     stmt.close();
   } catch (e)  {
