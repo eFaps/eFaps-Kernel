438b8e7b5dc8bdec550b15014fc87788e6da8e6e
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-09-05 21:47:33 +0000

    - showing type in the form - grouping creation and modification information
    git-svn-id: https://efaps.googlecode.com/svn/trunk@344 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- shell/src/main/resources/org/efaps/js/definitions/Admin/LifeCycle/UserInterfaces/FORM_Admin_LifeCycle_AccessTypeForm.js
+++ shell/src/main/resources/org/efaps/js/definitions/Admin/LifeCycle/UserInterfaces/FORM_Admin_LifeCycle_AccessTypeForm.js
@@ -1,23 +1,31 @@
-/*******************************************************************************
-* Admin_UI_Form:
-* ~~~~~~~~~~~~~~
-* Admin_LifeCycle_AccessTypeForm
-*
-* Description:
-* ~~~~~~~~~~~~
-*
-* History:
-* ~~~~~~~~
-* Revision: $Rev$
-* Date:     $Date$
-* By:       $Author$
-*
-* Author:
-* ~~~~~~~
-* TMO
-*******************************************************************************/
+/*
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
 
 with (FORM)  {
+  with (addField("type"))  {
+    addProperty("Creatable",              "false");
+    addProperty("Editable",               "false");
+    addProperty("Expression",             "Type");
+    addProperty("Searchable",             "false");
+    addProperty("ShowTypeIcon",           "true");
+  }
   with (addField("id"))  {
     addProperty("Columns",                "20");
     addProperty("Creatable",              "false");
@@ -33,6 +41,10 @@ with (FORM)  {
     addProperty("Required",               "true");
     addProperty("Searchable",             "true");
   }
+  with (addField("createGroup"))  {
+    addProperty("Creatable",              "false");
+    addProperty("GroupCount",             "2");
+  }
   with (addField("creator"))  {
     addProperty("Creatable",              "false");
     addProperty("Editable",               "false");
@@ -44,6 +56,10 @@ with (FORM)  {
     addProperty("Editable",               "false");
     addProperty("Expression",             "Created");
     addProperty("Searchable",             "true");
+  }
+  with (addField("modifyGroup"))  {
+    addProperty("Creatable",              "false");
+    addProperty("GroupCount",             "2");
   }
   with (addField("modifier"))  {
     addProperty("Creatable",              "false");
