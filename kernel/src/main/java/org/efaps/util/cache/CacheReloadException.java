693b964d6b54f6573a3e26890001f2f07cb33342
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-10-20 10:14:30 +0000

    - add a common interface to reload a single cache
    git-svn-id: https://efaps.googlecode.com/svn/trunk@464 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ kernel/src/main/java/org/efaps/util/cache/CacheReloadException.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.util.cache;
+
+/**
+ * The <code>CacheReloadException</code> class is an exception is thrown, if 
+ * a cache implementing {@link CacheReloadInterface} could not be reloaded.
+ *
+ * @author tmo
+ * @version $Id$
+ * @see CacheReloadInterface
+ */
+public class CacheReloadException extends Exception  {
+
+  /**
+   * Constructs a new exception with the specified detail message. The cause is 
+   * not initialized, and may subsequently be initialized by a call to 
+   * {@link Throwable.initCause(Throwable)}.
+   *
+   * @param _message  the detail message (which is saved for later retrieval by
+   *                  the {@link Throwable.getMessage} method).
+   */
+  public CacheReloadException(final String _message) {
+    this(_message, null);
+  }
+
+  /**
+   * Constructs a new exception with the specified detail message and cause.
+   * <br/>
+   * Note that the detail message associated with cause is not automatically 
+   * incorporated in this exception's detail message.
+   *
+   * @param _message  the detail message (which is saved for later retrieval by
+   *                  the {@link Throwable.getMessage} method).
+   * @param _cause    the cause (which is saved for later retrieval by the 
+   *                  {@link Throwable.getCause} method). (A null value is 
+   *                  permitted, and indicates that the cause is nonexistent or 
+   *                  unknown.)
+   */
+  public CacheReloadException(final String _message, final Throwable _cause) {
+    super(_message, _cause);
+  }
+}
---
+++ kernel/src/main/java/org/efaps/util/cache/CacheReloadInterface.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.util.cache;
+
+/**
+ * @author tmo
+ * @version $Id$
+ * @todo description
+ */
+public interface CacheReloadInterface  {
+  
+  /**
+   * The enum defines the priority numbers for the caches used to initialise
+   * the kernel.
+   */
+  public static enum Priority  {
+    JAASSystem(100),
+    Role(200),
+    Group(300),
+    AttributeType(400),
+    SQLTable(500),
+    Type(600),
+    Attribute(700),
+    AccessType(800),
+    AccessSet(900),
+    EventDefinition(1000);
+    
+    /** Stores the priority number. */
+    public final int number;
+    
+    private Priority(final int _number)  {
+      this.number = _number;
+    }
+  }
+
+  /**
+   * Returns the priority number used to define when this cache is reloaded.
+   * E.g. the cache for attribute types must be loaded before the cache of 
+   * types.
+   *
+   * @return priority number 
+   */
+  public int priority();
+  
+  /**
+   * The cache is reloaded or initialised.
+   *
+   * @throws CacheException if the cache could not be reloaed
+   */
+  public void reloadCache() throws CacheReloadException;
+}
