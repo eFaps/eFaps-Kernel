0620fdbdb76c4051f04fa5e506c8cae7123b3934
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-10-20 22:33:56 +0000

    - webdav implementation for the root directory (/eFaps/webdav)
    git-svn-id: https://efaps.googlecode.com/svn/trunk@478 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ webapp/src/main/java/org/efaps/webdav/WebDAVImpl.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.webdav;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.efaps.db.Instance;
+import org.efaps.db.SearchQuery;
+import org.efaps.util.EFapsException;
+
+/**
+ *
+ * @author tmo
+ * @version $Id$
+ * @todo description
+ */
+public class WebDAVImpl implements WebDAVInterface  {
+  
+  /////////////////////////////////////////////////////////////////////////////
+  // static variables
+
+  /**
+   * Logging instance used in this class.
+   */
+  private static final Log LOG = LogFactory.getLog(WebDAVImpl.class);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // instance methods
+
+  /**
+   * @todo hard coded webdav impl
+   */
+  public List < AbstractResource > getSubs(final CollectionResource _collection)   {
+    
+    List < AbstractResource > subs = new ArrayList < AbstractResource > ();
+
+    try  {
+      SearchQuery query = new SearchQuery();
+      query.setQueryTypes("Admin_Integration_WebDAV");
+      query.addSelect("OID");
+      query.addSelect("Path");
+      query.addSelect("Modified");
+      query.addSelect("Created");
+      query.execute();
+      while (query.next())  {
+        String path = query.get("Path").toString();
+        subs.add(new CollectionResource(
+            new TeamCenterWebDAVImpl(),
+            path,
+            null,
+            (Date) query.get("Created"),
+            (Date) query.get("Modified"),
+            path
+        ));
+      }
+      query.close();
+    } catch (Exception e)  {
+      LOG.error("could not get all WebDAV integrations", e);
+    }
+
+    return subs;
+  }
+
+
+  /**
+   * @param _collection collection resource representing the folder (if null,
+   *                    means root folder)
+   * @param _name       name of the searched collection resource
+   * @return found collection resource for given instance or null if not found.
+   * @todo use EFapsException instead of Exception
+   */
+  public CollectionResource getCollection(final CollectionResource _collection,
+                                          final String _name)  {
+    
+    CollectionResource collection = null;
+    
+    try  {
+      SearchQuery query = new SearchQuery();
+      query.setQueryTypes("Admin_Integration_WebDAV");
+      query.addWhereExprEqValue("Path", _name);
+      query.addSelect("Created");
+      query.addSelect("Modified");
+      query.execute();
+      if (query.next())  {
+        collection = new CollectionResource(
+            new TeamCenterWebDAVImpl(),
+            _name,
+            null,
+            (Date) query.get("Created"),
+            (Date) query.get("Modified"),
+            _name
+        );
+      }
+      query.close();
+    } catch (Exception e)  {
+      LOG.error("could not get information about collection "
+                + "'" + _name + "'", e);
+    }
+    return collection;
+  }
+
+  /**
+   * @param _collection collection resource representing the folder (if null,
+   *                    means root folder)
+   * @param _name       name of the searched source resource
+   * @return found source resource for given instance or null if not found.
+   * @todo use EFapsException instead of Exception
+   */
+  public SourceResource getSource(final CollectionResource _collection,
+                                  final String _name)  {
+    
+    return null;
+  }
+
+
+  public boolean deleteCollection(final CollectionResource _collection)  {
+    return false;
+  }
+  
+  public boolean deleteSource(final SourceResource _source)  {
+    return false;
+  }
+
+  public boolean createCollection(final CollectionResource _collection, 
+                                  final String _name)  {
+    return false;
+  }
+
+  public boolean createSource(final CollectionResource _collection, 
+                              final String _name)  {
+    return false;
+  }
+
+  
+  public boolean checkinSource(final SourceResource _source, 
+                               final InputStream _inputStream)  {
+    return false;
+  }
+
+  public boolean checkoutSource(final SourceResource _source, 
+                                final OutputStream _outputStream)  {
+    return false;
+  }
+}
