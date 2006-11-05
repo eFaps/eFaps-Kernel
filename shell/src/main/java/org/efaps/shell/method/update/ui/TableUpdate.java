92f8c8875f4af7ee9566bb6b35a55d191a404ecd
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2006-11-05 12:32:34 +0000

    - moved logic for updateing table in collecion update (needed for reuse for form xml update)
    git-svn-id: https://efaps.googlecode.com/svn/trunk@545 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- shell/src/main/java/org/efaps/shell/method/update/ui/TableUpdate.java
+++ shell/src/main/java/org/efaps/shell/method/update/ui/TableUpdate.java
@@ -48,30 +48,7 @@ import org.efaps.util.EFapsException;
  * @version $Id$
  * @todo description
  */
-public class TableUpdate extends AbstractUpdate  {
-
-  /////////////////////////////////////////////////////////////////////////////
-  // static variables
-
-  /**
-   * Logging instance used to give logging information of this class.
-   */
-  private final static Log LOG = LogFactory.getLog(TableUpdate.class);
-
-  /** Link from field to icon */
-  private final static Link LINKFIELD2ICON
-                    = new Link("Admin_UI_LinkIcon", 
-                               "From", 
-                               "Admin_UI_Image", "To");
-
-  private final static Set <Link> ALLLINKS = new HashSet < Link > ();  {
-/*    ALLLINKS.add(LINK2ACCESSTYPE);
-    ALLLINKS.add(LINK2DATAMODELTYPE);
-    ALLLINKS.add(LINK2PERSON);
-    ALLLINKS.add(LINK2ROLE);
-    ALLLINKS.add(LINK2GROUP);
-*/
-  }
+public class TableUpdate extends AbstractCollectionUpdate  {
 
   /////////////////////////////////////////////////////////////////////////////
   // constructors
@@ -80,7 +57,7 @@ public class TableUpdate extends AbstractUpdate  {
    *
    */
   public TableUpdate() {
-    super("Admin_UI_Table", ALLLINKS);
+    super("Admin_UI_Table");
   }
 
   /////////////////////////////////////////////////////////////////////////////
@@ -93,186 +70,8 @@ public class TableUpdate extends AbstractUpdate  {
   }
 
   public static TableUpdate readXMLFile(final File _file) throws IOException  {
-    TableUpdate ret = null;
-
-    try  {
-      Digester digester = new Digester();
-      digester.setValidating(false);
-      digester.addObjectCreate("ui-table", TableUpdate.class);
-
-      digester.addCallMethod("ui-table/uuid", "setUUID", 1);
-      digester.addCallParam("ui-table/uuid", 0);
-
-      digester.addObjectCreate("ui-table/definition", Definition.class);
-      digester.addSetNext("ui-table/definition", "addDefinition");
-
-      digester.addCallMethod("ui-table/definition/version", "setVersion", 4);
-      digester.addCallParam("ui-table/definition/version/application", 0);
-      digester.addCallParam("ui-table/definition/version/global", 1);
-      digester.addCallParam("ui-table/definition/version/local", 2);
-      digester.addCallParam("ui-table/definition/version/mode", 3);
-      
-      digester.addCallMethod("ui-table/definition/name", "setName", 1);
-      digester.addCallParam("ui-table/definition/name", 0);
-
-      digester.addCallMethod("ui-table/definition/property", "addProperty", 2);
-      digester.addCallParam("ui-table/definition/property", 0, "name");
-      digester.addCallParam("ui-table/definition/property/value", 1);
-
-      digester.addObjectCreate("ui-table/definition/field", Field.class);
-      digester.addSetNext("ui-table/definition/field", "addField");
-
-      digester.addCallMethod("ui-table/definition/field", "setName", 1);
-      digester.addCallParam("ui-table/definition/field", 0, "name");
-
-      digester.addCallMethod("ui-table/definition/field/icon", "setIcon", 1);
-      digester.addCallParam("ui-table/definition/field/icon", 0);
-
-      digester.addCallMethod("ui-table/definition/field/property", "addProperty", 2);
-      digester.addCallParam("ui-table/definition/field/property", 0, "name");
-      digester.addCallParam("ui-table/definition/field/property/value", 1);
-
-      ret = (TableUpdate) digester.parse(_file);
-    } catch (SAXException e)  {
-e.printStackTrace();
-      //      LOG.error("could not read file '" + _fileName + "'", e);
-    }
-    return ret;
-  }
-
-  /////////////////////////////////////////////////////////////////////////////
-  // class for a field
-
-  public static class Field  {
-    
-    /** Name of the field. */
-    private String name = null;
-    
-    /** Icon of the field. */
-    private String icon = null;
-    
-    /**
-     * Property value depending on the property name for this field of a 
-     * definition.
-     *
-     * @see #addProperty.
-     */
-    private final Map < String, String > properties 
-                                          = new HashMap < String, String > ();
-
-    /**
-     * @see #name
-     */
-    public void setName(final String _name)  {
-      this.name = _name;
-    }
-
-    /**
-     * Add a new property with given name and value to this definition.
-     *
-     * @param _name   name of the property to add
-     * @param _value  value of the property to add
-     * @see #properties
-     */
-    public void addProperty(final String _name, final String _value)  {
-      this.properties.put(_name, _value);
-    }
-
-    /**
-     *
-     */
-    public void setIcon(final String _icon)  {
-      this.icon = _icon;
-    }
-
-    /**
-     * Returns a string representation with values of all instance variables
-     * of a field.
-     *
-     * @return string representation of this definition of a column
-     */
-    public String toString()  {
-      return new ToStringBuilder(this)
-        .append("name",       this.name)
-        .append("properties", this.properties)
-        .toString();
-    }
-  }
-  
-  
-  /////////////////////////////////////////////////////////////////////////////
-  // class for the definitions
-
-  public static class Definition extends DefinitionAbstract {
-
-    /** All fields for the table are stored in this variable */
-    private List < Field > fields = new ArrayList < Field > ();
-    
-    ///////////////////////////////////////////////////////////////////////////
-    // instance methods
-
-    /**
-     *
-     */
-    public Instance updateInDB(final Instance _instance,
-                           final Set < Link > _allLinkTypes,
-                           final Insert _insert) throws EFapsException, Exception  {
-
-      Instance instance = super.updateInDB(_instance, _allLinkTypes, _insert);
-
-      // cleanup fields (remove all fields from table)
-      SearchQuery query = new SearchQuery();
-      query.setExpand(instance, "Admin_UI_Field\\Collection");
-      query.addSelect("OID");
-      query.executeWithoutAccessCheck();
-      while (query.next())  {
-        Instance field = new Instance((String) query.get("OID"));
-        setPropertiesInDb(field, null);
-        setLinksInDB(field, LINKFIELD2ICON, null);
-        (new Delete(field)).executeWithoutAccessCheck();
-      }
-      query.close();
-
-      // append new fields
-      for (Field field : this.fields)  {
-        Insert insert = new Insert("Admin_UI_Field");
-        insert.add("Collection", "" + instance.getId());
-        insert.add("Name",       field.name);
-        insert.executeWithoutAccessCheck();
-        setPropertiesInDb(insert.getInstance(), field.properties);
-        Map < String, Map < String, String > > iconsMap
-                          = new HashMap < String, Map < String, String > > ();
-        if (field.icon != null)  {
-          iconsMap.put(field.icon, null);
-        }
-        setLinksInDB(insert.getInstance(), LINKFIELD2ICON, iconsMap);
-      }
-      
-      return instance;
-    }
-
-    /**
-     * Adds a new field to this definition of the table.
-     *
-     * @param _field new field to add to this table
-     * @see #fields
-     * @see #Field
-     */
-    public void addField(final Field _field)  {
-      this.fields.add(_field);
-    }
-
-    /**
-     * Returns a string representation with values of all instance variables
-     * of a field.
-     *
-     * @return string representation of this definition of a column
-     */
-    public String toString()  {
-      return new ToStringBuilder(this)
-        .appendSuper(super.toString())
-        .append("fields",       this.fields)
-        .toString();
-    }
+    return (TableUpdate) AbstractCollectionUpdate.readXMLFile(_file, 
+                                                              "ui-table", 
+                                                              TableUpdate.class);
   }
 }
