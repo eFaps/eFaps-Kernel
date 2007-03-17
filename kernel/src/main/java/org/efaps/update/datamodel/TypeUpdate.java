75c637c8cb045330f3104fd9a61ffa8184fe4fe2
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2007-03-17 15:28:04 +0000

    - the parent type id is evaluated now before the update is done (and not when the parent type is defined)
    git-svn-id: https://efaps.googlecode.com/svn/trunk@720 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel/src/main/java/org/efaps/update/datamodel/TypeUpdate.java
+++ kernel/src/main/java/org/efaps/update/datamodel/TypeUpdate.java
@@ -303,6 +303,23 @@ e.printStackTrace();
     ///////////////////////////////////////////////////////////////////////////
     // instance variables
     
+    /**
+     * Stores the name of the parent type. The parent type could not be
+     * evaluated because it could be that the type does not exists (and so
+     * the type id is evaluated before the insert / update from method
+     * {@link #updateInDB}).
+     *
+     * @see #setParent
+     * @see #updateInDB
+     */
+    private String parentType = null;
+
+    /**
+     * All attributes of the type are stored in this list.
+     *
+     * @see #updateInDB
+     * @see #addAttribute
+     */
     private final List < Attribute > attributes 
                                               = new ArrayList < Attribute > ();
     
@@ -310,31 +327,24 @@ e.printStackTrace();
     // instance methods
 
     /**
+     * If a parent type in {@link #parentType} is defined, the type id is
+     * evaluated and added to attributes to update (if no parent type is
+     * defined, the parent type id is set to <code>null</code>).
      * After the type is updated (or inserted if needed), all attributes must
      * be updated.
      *
      * @todo throw Exception is not allowed
+     * @see #parentType
+     * @see #attributes
      */
     public Instance updateInDB(final Instance _instance,
                                final Set < Link > _allLinkTypes,
                                final Insert _insert) throws EFapsException, Exception  {
-
-      Instance instance = super.updateInDB(_instance, _allLinkTypes, _insert);
-
-      for (Attribute attr : this.attributes)  {
-        attr.updateInDB(instance, getValue("Name"));
-      }
-      return instance;
-    }
-
-    /**
-     */
-    public void setParent(final String _parent) throws EFapsException {
-      if ((_parent != null) && (_parent.length() > 0))  {
-        // search for the instance
+      // set the id of the parent type (if defined)
+      if ((this.parentType != null) && (this.parentType.length() > 0))  {
         SearchQuery query = new SearchQuery();
         query.setQueryTypes("Admin_DataModel_Type");
-        query.addWhereExprEqValue("Name", _parent);
+        query.addWhereExprEqValue("Name", this.parentType);
         query.addSelect("OID");
         query.executeWithoutAccessCheck();
         if (query.next())  {
@@ -347,6 +357,23 @@ e.printStackTrace();
       } else  {
         addValue("ParentType", null);
       }
+
+      Instance instance = super.updateInDB(_instance, _allLinkTypes, _insert);
+
+      for (Attribute attr : this.attributes)  {
+        attr.updateInDB(instance, getValue("Name"));
+      }
+      return instance;
+    }
+
+    /**
+     * Setter method for instance variable {@link #parentType}.
+     *
+     * @param _parentType new value to set
+     * @see #parentType
+     */
+    public void setParent(final String _parentType)  {
+      this.parentType = _parentType;
     }
     
     public void addAttribute(final String _name,
