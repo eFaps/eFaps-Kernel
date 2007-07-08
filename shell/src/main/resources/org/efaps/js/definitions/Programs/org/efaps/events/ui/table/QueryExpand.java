bacbcd4fc74e740d963c45f51dc7b9eeb88f3523
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2007-07-08 20:57:02 +0000

    - set correct header
    git-svn-id: https://efaps.googlecode.com/svn/trunk@963 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- shell/src/main/resources/org/efaps/js/definitions/Programs/org/efaps/events/ui/table/QueryExpand.java
+++ shell/src/main/resources/org/efaps/js/definitions/Programs/org/efaps/events/ui/table/QueryExpand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2007 The eFaps Team
+ * Copyright 2003-2007 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Revision:        $Rev:851 $
- * Last Changed:    $Date:2007-06-02 12:36:03 -0500 (Sat, 02 Jun 2007) $
- * Last Changed By: $Author:jmo $
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
  */
 
 package org.efaps.events.ui.table;
