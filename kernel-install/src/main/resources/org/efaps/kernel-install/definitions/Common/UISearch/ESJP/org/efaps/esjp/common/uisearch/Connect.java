8c5cc28ca9626160983145690beda9c99e54a3f7
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2007-07-22 11:53:17 +0000

    - new ESJP for the connect (after search) - remove obsolete search menus (directly in the search defined) - remove search commands (original search commands are reused) - remove search bean (was from V0.6 and older -> replaced by execute connect)
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1137 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ kernel-install/src/main/resources/org/efaps/kernel-install/definitions/Common/UISearch/ESJP/org/efaps/esjp/common/uisearch/Connect.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2003-2007 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.common.uisearch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.efaps.admin.event.Parameter.ParameterValues;
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return.ReturnValues;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.ui.CommandAbstract;
+import org.efaps.db.Insert;
+import org.efaps.db.Instance;
+import org.efaps.db.SearchQuery;
+import org.efaps.util.EFapsException;
+
+/**
+ * @author tmo
+ * @version $Id$
+ * @todo description
+ */
+public class Connect  {
+  /**
+   * Logger for this class
+   */
+  private static final Log LOG = LogFactory.getLog(Connect.class);
+
+  public Return execute(final Parameter _parameter) throws EFapsException {
+    Return ret = new Return();
+
+    CommandAbstract command = (CommandAbstract) _parameter.get(ParameterValues.UIOBJECT);
+    Map properties = (Map) _parameter.get(ParameterValues.PROPERTIES);
+    Instance parent = (Instance) _parameter.get(ParameterValues.INSTANCE);
+    String childOids[] = (String[]) _parameter.get(ParameterValues.OTHERS);
+
+    String type = (String) properties.get("ConnectType");
+    String childAttr = (String) properties.get("ConnectChildAttribute");
+    String parentAttr = (String) properties.get("ConnectParentAttribute");
+
+/*      if (childAttr == null) {
+        throw new Exception("Could not found child attribute '"
+            + command.getConnectChildAttribute() + "' for type '"
+            + type.getName() + "'");
+      }
+      if (parentAttr == null) {
+        throw new Exception("Could not found parent attribute '"
+            + command.getConnectParentAttribute() + "' for type '"
+            + type.getName() + "'");
+      }
+*/
+    for (String childOid : childOids)  {
+      Instance child = new Instance(childOid);
+      Insert insert = new Insert(type);
+      insert.add(parentAttr, "" + parent.getId());
+      insert.add(childAttr, "" + child.getId());
+      insert.execute();
+    }
+
+    return ret;
+  }
+}
