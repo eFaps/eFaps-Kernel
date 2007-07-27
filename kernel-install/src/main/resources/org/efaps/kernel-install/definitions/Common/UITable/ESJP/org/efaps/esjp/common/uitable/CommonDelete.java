22a224c19d857af1ef3797988968e7d751b9abcc
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2007-07-27 16:03:53 +0000

    - eFaps exception is thrown if delete fails - 'DeleteIndex' property is set on the event definition (and not on command level)
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1191 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel-install/src/main/resources/org/efaps/kernel-install/definitions/Common/UITable/ESJP/org/efaps/esjp/common/uitable/CommonDelete.java
+++ kernel-install/src/main/resources/org/efaps/kernel-install/definitions/Common/UITable/ESJP/org/efaps/esjp/common/uitable/CommonDelete.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2007 The eFaps Team
+ * Copyright 2003-2007 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,52 +20,59 @@
 
 package org.efaps.esjp.common.uitable;
 
-import java.util.StringTokenizer;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.util.Map;
 
 import org.efaps.admin.event.EventExecution;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Parameter.ParameterValues;
-import org.efaps.admin.ui.Command;
 import org.efaps.db.Delete;
 import org.efaps.util.EFapsException;
 
+/**
+ * The ESJP is used to delete selected objects in a row of a web table.
+ *
+ * <b>Example:</b><br/>
+ * <code>
+ *   &lt;execute program="org.efaps.esjp.common.uitable.CommonDelete"&gt;
+ *     &lt;property name="DeleteIndex"&gt;1&lt;/property&gt;
+ *   &lt;/execute&gt;
+ * </code>
+ * Because an expand is made to show the web table, the oid with the index 1 in
+ * the list of oids is used to delete.
+ *
+ * @author tmo
+ * @version $Id$
+ * @todo description
+ */
 public class CommonDelete implements EventExecution {
+
   /**
-   * Logger for this class
+   * All selected oids are split by the pipe (<code>|</code> - meaning the oids
+   * on the way to the row oid, like an expand) and deletes - depending on the
+   * delete index - this oid.
+   *
+   * @param _parameter  parameters from the submitted web table
+   * @throws EFapsException if a delete of the selected oids is not possible
    */
-  private static final Log LOG = LogFactory.getLog(CommonDelete.class);
+  public Return execute(Parameter _parameter) throws EFapsException  {
+    String[] allOids = (String[]) _parameter.get(ParameterValues.OTHERS);
+    Map properties = (Map) _parameter.get(ParameterValues.PROPERTIES);
 
-  public Return execute(Parameter _parameter) {
-    String[] oids = (String[]) _parameter.get(ParameterValues.OTHERS);
-    Command command = (Command) _parameter.get(ParameterValues.UIOBJECT);
+    if (allOids != null)  {
 
-    if (oids != null) {
-      for (int i = 0; i < oids.length; i++) {
-        String oid = oids[i];
-        if (command.getDeleteIndex() > 0) {
-          StringTokenizer tokens = new StringTokenizer(oid, "|");
-          int count = 0;
-          while (tokens.hasMoreTokens() && count < command.getDeleteIndex()) {
-            oid = tokens.nextToken();
-            count++;
-
-          }
-        }
-        Delete del = new Delete(oid);
-        try {
-          del.execute();
-        } catch (EFapsException e) {
-          LOG.error("execute(Parameter)", e);
-        }
-
+      int delIdx = 0;
+      String delIdxStr = (String) properties.get("DeleteIndex");
+      if ((delIdxStr != null) && (delIdxStr.length() > 0))  {
+        delIdx = Integer.parseInt(delIdxStr);
       }
 
+      for (String rowOids : allOids) {
+        String[] colOids = rowOids.split("\\|");
+        (new Delete(colOids[delIdx])).execute();
+      }
     }
-    return null;
 
+    return null;
   }
 }
