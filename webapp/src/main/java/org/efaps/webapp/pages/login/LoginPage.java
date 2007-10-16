6c9e3f4e37cbf514f0b09463aa1db4e676e95f17
jmox <jmox@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2007-10-16 16:17:10 +0000

    - the LoginPage now shows a Message to the User in case of an invalid Login
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1497 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- webapp/src/main/java/org/efaps/webapp/pages/login/LoginPage.java
+++ webapp/src/main/java/org/efaps/webapp/pages/login/LoginPage.java
@@ -3,10 +3,12 @@ package org.efaps.webapp.pages.login;
 import org.apache.wicket.Session;
 import org.apache.wicket.behavior.StringHeaderContributor;
 import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.resources.StyleSheetReference;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.string.JavascriptUtils;
 
@@ -16,16 +18,34 @@ import org.efaps.webapp.EFapsSession;
 import org.efaps.webapp.pages.main.MainPage;
 
 /**
+ * This class renders the LoginPage for the eFpas-WebApplication.<br>
+ * It is called from the #
+ * {@link #onRuntimeException(org.efaps.webapp.EFapsWebRequestCycle)} method, in
+ * the case that noone is logged in. In case of a wrong login try, an additional
+ * Message is shwon to the User.
+ *
  * @author jmox
  * @version $Id$
- *
  */
 public class LoginPage extends WebPage implements
     EFapsNoAuthorizationNeededInterface {
 
   private static final long serialVersionUID = 524408099967362477L;
 
+  /**
+   * standart Constructor showing no Message
+   */
   public LoginPage() {
+    this(false);
+  }
+
+  /**
+   * Constructor showing a "wrong Login Message" depending on the Parameter
+   *
+   * @param _msg
+   *                true, if "wrong Login Message: should be shown, else false
+   */
+  public LoginPage(final boolean _msg) {
 
     this.add(new StringHeaderContributor(""
         + JavascriptUtils.SCRIPT_OPEN_TAG
@@ -35,20 +55,24 @@ public class LoginPage extends WebPage implements
         + "  }\n"
         + "}\n"
         + JavascriptUtils.SCRIPT_CLOSE_TAG));
+
+    this.add(new StyleSheetReference("css", getClass(), "LoginPage.css"));
+
     Form form = new Form("form") {
 
       private static final long serialVersionUID = 1L;
 
       @Override
-      protected void onComponentTag(final ComponentTag _tag) {
-        super.onComponentTag(_tag);
-      }
-
-      @Override
       protected void onSubmit() {
         super.onSubmit();
-        ((EFapsSession) this.getSession()).checkin();
-        this.getRequestCycle().setResponsePage(MainPage.class);
+        EFapsSession session = ((EFapsSession) this.getSession());
+        session.checkin();
+        if (session.isLogedIn()) {
+          this.getRequestCycle().setResponsePage(MainPage.class);
+        } else {
+          LoginPage page = new LoginPage(true);
+          this.getRequestCycle().setResponsePage(page);
+        }
       }
 
     };
@@ -80,5 +104,11 @@ public class LoginPage extends WebPage implements
     button.add(new Label("formbuttonlabel", new Model(DBProperties.getProperty(
         "Login.Button.Label", Session.get().getLocale().getLanguage()))));
 
+    if (_msg) {
+      this.add(new Label("msg", new Model(DBProperties.getProperty(
+          "Login.Wrong.Label", Session.get().getLocale().getLanguage()))));
+    } else {
+      this.add(new WebMarkupContainer("msg").setVisible(false));
+    }
   }
 }
