e5742e1b481b9630d5bff401ff9cbf1548b2aa3a
jmox <jmox@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2007-10-27 03:20:00 +0000

    - commit due to a previous  commiterror
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1551 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ webapp/src/main/java/org/efaps/ui/wicket/components/button/ButtonStyleBehavior.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2003-2007 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.ui.wicket.components.button;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.behavior.AbstractBehavior;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.IHeaderResponse;
+
+/**
+ * @author jmox
+ * @version $Id$
+ */
+public class ButtonStyleBehavior extends AbstractBehavior {
+
+  private static final long serialVersionUID = 1L;
+
+  public final static ResourceReference CSS =
+      new ResourceReference(ButtonStyleBehavior.class, "ButtonBehavior.css");
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see org.apache.wicket.behavior.AbstractBehavior#renderHead(org.apache.wicket.markup.html.IHeaderResponse)
+   */
+  @Override
+  public void renderHead(final IHeaderResponse _response) {
+    super.renderHead(_response);
+    _response.renderCSSReference(CSS);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see org.apache.wicket.behavior.AbstractBehavior#onComponentTag(org.apache.wicket.Component,
+   *      org.apache.wicket.markup.ComponentTag)
+   */
+  @Override
+  public void onComponentTag(final Component _component, final ComponentTag _tag) {
+    super.onComponentTag(_component, _tag);
+    _tag.put("class", "eFapsButton");
+  }
+
+}
