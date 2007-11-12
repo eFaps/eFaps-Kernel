88e6a7f4d176a7bb9299b47e2e47ded5c1a4b7cc
jmox <jmox@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2007-11-12 01:02:00 +0000

    - due tue the size of the Javascript it is moved into a external JavaScript-File
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1616 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- webapp/src/main/java/org/efaps/ui/wicket/components/table/header/HeaderPanel.java
+++ webapp/src/main/java/org/efaps/ui/wicket/components/table/header/HeaderPanel.java
@@ -23,6 +23,7 @@ package org.efaps.ui.wicket.components.table.header;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.HeaderContributor;
@@ -31,6 +32,7 @@ import org.apache.wicket.behavior.StringHeaderContributor;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.html.resources.JavascriptResourceReference;
 import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.protocol.http.request.WebClientInfo;
 import org.apache.wicket.util.string.CssUtils;
@@ -43,6 +45,7 @@ import org.efaps.ui.wicket.components.modalwindow.UpdateParentCallback;
 import org.efaps.ui.wicket.models.HeaderModel;
 import org.efaps.ui.wicket.models.TableModel;
 import org.efaps.ui.wicket.models.TableModel.UserAttributeKey;
+import org.efaps.ui.wicket.pages.error.ErrorPage;
 import org.efaps.util.EFapsException;
 
 /**
@@ -55,6 +58,9 @@ public class HeaderPanel extends Panel {
 
   private final ModalWindowContainer modal =
       new ModalWindowContainer("eFapsModal");
+
+  public static final JavascriptResourceReference JAVASCRIPT =
+      new JavascriptResourceReference(HeaderPanel.class, "HeaderPanel.js");
 
   AjaxStoreColumnWidth ajaxstore;
 
@@ -138,6 +144,8 @@ public class HeaderPanel extends Panel {
 
     this.add(new HeaderContributor(HeaderContributor
         .forJavaScript(DojoReference.JS_DOJO)));
+    this
+        .add(new HeaderContributor(HeaderContributor.forJavaScript(JAVASCRIPT)));
   }
 
   public final ModalWindowContainer getModal() {
@@ -148,144 +156,8 @@ public class HeaderPanel extends Panel {
 
     final String ret =
         JavascriptUtils.SCRIPT_OPEN_TAG
-            + "  window.onresize = eFapsPositionTableHeader;\n"
-            + "  window.onload = eFapsPositionTableHeader;\n"
-            + "  function eFapsPositionTableHeader() {\n"
-            + "    var header = document.getElementById(\"eFapsTableHeader\");\n"
-            + "    var cells = new Array();\n"
-            + "    var widthWeight = 0;\n"
-            + "    var widthCor = 0;\n"
-            + "    var addCell = 0;\n"
-            + "    var celldivs = header.getElementsByTagName(\"div\");\n"
-            + "    for(i = 0;i<celldivs.length;i++){\n"
-            + "      var cell = celldivs[i];\n"
-            + "      var fixed = cell.className.indexOf(\"eFapsCellFixedWidth\");\n"
-            + "      if(fixed > -1){\n"
-            + "        var addwith = getAdditionalWidth(cell);\n"
-            + "        cells.push(new Array(cell.clientWidth, addwith,false));\n"
-            + "        widthCor += cell.clientWidth + addwith;\n"
-            + "      }\n"
-            + "      var f = cell.className.indexOf(\"eFapsCellWidth\");\n"
-            + "      if (f>-1){\n"
-            + "        var addwith = getAdditionalWidth(cell);\n"
-            + "        cells.push(new Array(cell.clientWidth, addwith,true));\n"
-            + "        widthWeight += cell.clientWidth;\n"
-            + "        widthCor+= addwith;\n"
-            + "      }\n"
-            + "    }\n"
-            + "    var tablebody = document.getElementById(\"eFapsTableBody\");\n"
-            + "    var completeWidth = (tablebody.clientWidth ) ;\n"
-            + "    if (completeWidth != 0) {\n"
-            + "      header.style.width = completeWidth + \"px\";\n"
-            + "      var cellWidth;\n"
-            + "      var rightshift = 0;\n"
-            + "      for(k = 0;k < cells.length; k++){\n"
-            + "        if(cells[k][2]==true){\n"
-            + "          var rule = getStyleRule(k);\n"
-            + "          cellWidth = ((100/widthWeight * cells[k][0])/100)* (completeWidth - widthCor - 5);\n"
-            + "          rule.style.width= cellWidth + \"px\";\n"
-            + "        }else {\n"
-            + "          cellWidth = cells[k][0];\n"
-            + "        }\n"
-            + "        if(k+1 < cells.length){\n"
-            + "          rightshift += cellWidth + cells[k][1];\n"
-            + "          if(cells[k][2]==true ){\n"
-            + "            if(document.getElementById(k+\"eFapsHeaderSeperator\")){\n"
-            + "              document.getElementById(k+\"eFapsHeaderSeperator\").style.left = rightshift + \"px\";\n"
-            + "            }\n"
-            + "          }\n"
-            + "        }\n"
-            + "      }\n"
-            + "    }\n"
-            + "  }\n"
-            + "  function getStyleRule(_styleIndex) {\n"
-            + "    var selectorName = \"div.eFapsCellWidth\" + _styleIndex;\n"
-            + "    for (i = 0; i < document.styleSheets.length; i++) { \n"
-            + "      if(document.styleSheets[i].title==\"eFapsTableWidthStyles\"){\n"
-            + "        for (j = 0; j < document.styleSheets[i].cssRules.length; j++) {\n"
-            + "          if (document.styleSheets[i].cssRules[j].selectorText == selectorName) {\n"
-            + "            return document.styleSheets[i].cssRules[j];\n"
-            + "          }\n"
-            + "        }\n"
-            + "      }\n"
-            + "    }\n"
-            + "  }\n"
-            + "  function getAdditionalWidth(_cell){\n"
-            + "    var compu = window.getComputedStyle(_cell,null); \n"
-            + "    var width=0;\n"
-            + "    width += parseInt(compu.getPropertyValue(\"margin-left\"));\n"
-            + "    width += parseInt(compu.getPropertyValue(\"margin-right\"));\n"
-            + "    width += parseInt(compu.getPropertyValue(\"padding-left\"));\n"
-            + "    width += parseInt(compu.getPropertyValue(\"padding-right\"));\n"
-            + "    width += parseInt(compu.getPropertyValue(\"border-left-width\"));\n"
-            + "    width += parseInt(compu.getPropertyValue(\"border-right-width\"));\n"
-            + "    return width;\n"
-            + "  }\n"
-            + "  var minWidth = 20;\n"
-            + "  var seperatorWidth = 200;\n"
-            + "  var lastpos= 0;\n"
-            + "  var startpos= 0;\n"
-            + "  var seperatorOffset= 0;\n"
-            + "  var connections = [];\n"
-            + "  var seperator;"
-            + "  function beginColumnSize(_seperator,_event){\n"
-            + "    sizing=true;\n"
-            + "    lastpos=_event.screenX;\n"
-            + "    startpos=lastpos;\n"
-            + "    seperator=_seperator;\n"
-            + "    seperatorOffset=_event.screenX- parseInt(_seperator.style.left);\n"
-            + "    _seperator.style.width = parseInt(window.getComputedStyle(_seperator,null).getPropertyValue(\"width\")) + seperatorWidth +\"px\";\n"
-            + "    _seperator.style.left = parseInt(_seperator.style.left) - seperatorWidth/2 +\"px\";\n"
-            + "    _seperator.style.backgroundPosition=\"top center\";\n"
-            + "    connections[0] = dojo.connect(_seperator,\"onmousemove\",this,  \"doColumnSize\" );\n"
-            + "    connections[1] = dojo.connect(_seperator,\"onmouseout\",this,  \"cancelColumnSize\" );\n"
-            + "  }\n"
-            + "  function doColumnSize(_event){\n"
-            + "    seperator.style.left= (_event.screenX-seperatorOffset) - seperatorWidth/2 +\"px\";\n"
-            + "    lastpos=_event.screenX;\n"
-            + "  }\n"
-            + "  function endColumnSize(_seperator,_event){\n"
-            + "    dojo.forEach(connections,dojo.disconnect);\n "
-            + "    var dif = lastpos - startpos;\n"
-            + "    var i = parseInt(seperator.id);\n"
-            + "    var leftrule = getStyleRule(i);\n"
-            + "    var rightrule = getStyleRule(i+1);\n"
-            + "    var leftWidth = parseInt(leftrule.style.width);\n"
-            + "    var rightWidth = parseInt(rightrule.style.width);\n"
-            + "    var move = 0;\n"
-            + "    if(leftWidth + dif > minWidth && rightWidth - dif > minWidth){\n"
-            + "      leftrule.style.width = leftWidth + dif +\"px\";\n"
-            + "      rightrule.style.width = rightWidth - dif +\"px\";\n"
-            + "    } else {\n"
-            + "      if(dif < 0){\n"
-            + "        leftrule.style.width = minWidth +\"px\";\n"
-            + "        rightrule.style.width = rightWidth + leftWidth - minWidth + \"px\";\n"
-            + "        move = (leftWidth + dif-minWidth);\n"
-            + "      } else {\n"
-            + "        rightrule.style.width = minWidth +\"px\";\n"
-            + "        leftrule.style.width = leftWidth + rightWidth - minWidth + \"px\";\n"
-            + "        move = -(rightWidth - dif - minWidth);"
-            + "      }\n"
-            + "    }\n"
-            + "    _seperator.style.width = parseInt(_seperator.style.width) - seperatorWidth +\"px\";\n"
-            + "    _seperator.style.left = parseInt(_seperator.style.left) - move + seperatorWidth/2 +\"px\";\n"
-            + "    _seperator.style.backgroundPosition=\"-200px 0\";\n"
-            + "storeColumnWidths(getColumnWidths());\n"
-            + "  }\n"
-            + "  function cancelColumnSize(_event){\n"
-            + "    endColumnSize(seperator,_event);\n"
-            + "  }\n"
-            + "  function getColumnWidths(){\n"
-            + "    var header = document.getElementById(\"eFapsTableHeader\");\n"
-            + "    var celldivs = header.getElementsByTagName(\"div\");\n"
-            + "    var widths=\"\";\n"
-            + "    for(i = 0;i<celldivs.length;i++){\n"
-            + "      if(celldivs[i].className.indexOf(\"eFapsCellFixedWidth\")>-1 || celldivs[i].className.indexOf(\"eFapsCellWidth\")>-1){\n"
-            + "        widths += window.getComputedStyle(celldivs[i],null).getPropertyValue(\"width\") +\";\";\n"
-            + "      }\n"
-            + "    }\n"
-            + "    return widths;\n"
-            + "  }\n"
+            + "  window.onresize = positionTableColumns;\n"
+            + "  window.onload = positionTableColumns;\n"
             + _ajaxstore.getJavaScript()
             + JavascriptUtils.SCRIPT_CLOSE_TAG;
 
@@ -354,22 +226,25 @@ public class HeaderPanel extends Panel {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * String used as Variablename in the Javascript
+     */
     public final static String COLUMNW_PARAMETERNAME = "eFapsColumnWidths";
 
     public String getJavaScript() {
       StringBuilder ret = new StringBuilder();
-      ret.append("function storeColumnWidths(_widths){\n").append(
+      ret.append("  function storeColumnWidths(_widths){\n    ").append(
           generateCallbackScript("wicketAjaxPost('"
               + getCallbackUrl(false)
               + "','"
               + COLUMNW_PARAMETERNAME
-              + "=' + _widths")).append("\n " + "}\n");
+              + "=' + _widths")).append("\n" + "  }\n");
       return ret.toString();
     }
 
     @Override
-    protected void respond(final AjaxRequestTarget arg0) {
-      String widths =
+    protected void respond(final AjaxRequestTarget _target) {
+      final String widths =
           this.getComponent().getRequest().getParameter(COLUMNW_PARAMETERNAME);
       try {
         Context.getThreadContext().setUserAttribute(
@@ -377,11 +252,8 @@ public class HeaderPanel extends Panel {
                 .getUserAttributeKey(UserAttributeKey.COLUMNWIDTH), widths);
         ((TableModel) this.getComponent().getModel()).resetModel();
       } catch (EFapsException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+        throw new RestartResponseException(new ErrorPage(e));
       }
-
     }
-
   }
 }
