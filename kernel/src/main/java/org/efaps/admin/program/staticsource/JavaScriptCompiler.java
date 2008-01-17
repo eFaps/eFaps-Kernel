785f691287159ce7432e0d87a6567ff1e4809201
jmox <jmox@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2008-01-17 22:10:19 +0000

    - removing comments and whitespaces from JavScripts
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1756 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel/src/main/java/org/efaps/admin/program/staticsource/JavaScriptCompiler.java
+++ kernel/src/main/java/org/efaps/admin/program/staticsource/JavaScriptCompiler.java
@@ -76,7 +76,7 @@ public class JavaScriptCompiler extends AbstractSourceCompiler {
       while ((thisLine = in.readLine()) != null) {
         if (!thisLine.contains(JavaScriptUpdate.ANNOTATION_VERSION)
             && !thisLine.contains(JavaScriptUpdate.ANNOTATION_EXTENDS)) {
-          buffer.append(thisLine);
+          buffer.append(thisLine).append("\n");
         }
       }
 
@@ -90,8 +90,7 @@ public class JavaScriptCompiler extends AbstractSourceCompiler {
       ret = buffer.toString();
       in.close();
       checkout.close();
-      ret = ret.replaceAll("\\s+", " ");
-      ret = ret.replaceAll("([!{}:;>+\\(\\[,])\\s+", "$1");
+      ret = stripCommentsAndWhitespace(ret);
       ret += "\n";
 
     } catch (final EFapsException e) {
@@ -103,6 +102,141 @@ public class JavaScriptCompiler extends AbstractSourceCompiler {
     }
     return ret;
 
+  }
+
+  private static int getPrevCount(String s, int fromIndex, char c) {
+    int count = 0;
+    --fromIndex;
+    while (fromIndex >= 0) {
+      if (s.charAt(fromIndex--) == c) {
+        ++count;
+      } else {
+        break;
+      }
+    }
+    return count;
+  }
+
+  public enum CurrentState {
+    LINE_COMMENT,
+    REGULAR_TEXT,
+    WHITE_SPACE,
+    MULTILINE_COMMENT,
+    STRING_SINGLE_QUOTE,
+    REG_EXP,
+    STRING_DOUBLE_QUOTES;
+  }
+
+  public String stripCommentsAndWhitespace(final String _org) {
+    // let's be optimistic
+    final StringBuffer result = new StringBuffer(_org.length() / 2);
+    CurrentState state = CurrentState.REGULAR_TEXT;
+
+    for (int i = 0; i < _org.length(); ++i) {
+      char c = _org.charAt(i);
+      final char next = (i < _org.length() - 1) ? _org.charAt(i + 1) : 0;
+      final char prev = (i > 0) ? _org.charAt(i - 1) : 0;
+
+      if (state == CurrentState.WHITE_SPACE) {
+
+        if (Character.isWhitespace(next) == false) {
+          state = CurrentState.REGULAR_TEXT;
+        }
+        continue;
+      }
+
+      if (state == CurrentState.REGULAR_TEXT) {
+        if (c == '/' && next == '/' && prev != '\\') {
+          state = CurrentState.LINE_COMMENT;
+          continue;
+        } else if (c == '/' && next == '*') {
+          state = CurrentState.MULTILINE_COMMENT;
+          ++i;
+          continue;
+        } else if (c == '/') {
+          // This might be a divide operator, or it might be a regular
+          // expression.
+          // Work out if it's a regular expression by finding the previous
+          // non-whitespace
+          // char, which
+          // will be either '=' or '('. If it's not, it's just a divide
+          // operator.
+          int idx = i - 1;
+          while (idx > 0) {
+            final char tmp = _org.charAt(idx);
+            if (Character.isWhitespace(tmp)) {
+              idx--;
+              continue;
+            }
+            if (tmp == '=' || tmp == '(') {
+              state = CurrentState.REG_EXP;
+              break;
+            }
+            break;
+          }
+        } else if (Character.isWhitespace(c) && Character.isWhitespace(next)) {
+          // ignore all whitespace characters after this one
+          state = CurrentState.WHITE_SPACE;
+          c = '\n';
+        } else if (c == '\'') {
+          state = CurrentState.STRING_SINGLE_QUOTE;
+        } else if (c == '"') {
+          state = CurrentState.STRING_DOUBLE_QUOTES;
+        }
+        result.append(c);
+        continue;
+      }
+
+      if (state == CurrentState.LINE_COMMENT) {
+        if (c == '\n' || c == '\r') {
+          state = CurrentState.REGULAR_TEXT;
+          continue;
+        }
+      }
+
+      if (state == CurrentState.MULTILINE_COMMENT) {
+        if (c == '*' && next == '/') {
+          state = CurrentState.REGULAR_TEXT;
+          ++i;
+          continue;
+        }
+      }
+
+      if (state == CurrentState.STRING_SINGLE_QUOTE) {
+        // to leave a string expression we need even (or zero) number of
+        // backslashes
+        final int count = getPrevCount(_org, i, '\\');
+        if (c == '\'' && count % 2 == 0) {
+          state = CurrentState.REGULAR_TEXT;
+        }
+        result.append(c);
+        continue;
+      }
+
+      if (state == CurrentState.STRING_DOUBLE_QUOTES) {
+        // to leave a string expression we need even (or zero) number of
+        // backslashes
+        final int count = getPrevCount(_org, i, '\\');
+        if (c == '"' && count % 2 == 0) {
+          state = CurrentState.REGULAR_TEXT;
+        }
+        result.append(c);
+        continue;
+      }
+
+      if (state == CurrentState.REG_EXP) {
+        // to leave regular expression we need even (or zero) number of
+        // backslashes
+        final int count = getPrevCount(_org, i, '\\');
+        if (c == '/' && count % 2 == 0) {
+          state = CurrentState.REGULAR_TEXT;
+        }
+        result.append(c);
+        continue;
+      }
+    }
+
+    return result.toString();
   }
 
   @Override
