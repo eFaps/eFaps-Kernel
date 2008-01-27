4aff56ba98b2515753001c4e83e524b9a61302a5
jmox <jmox@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2008-01-27 19:18:34 +0000

    - replacing name with UUID to access a Type - JavaDoc
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1772 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel/src/main/java/org/efaps/admin/program/bundle/BundleMaker.java
+++ kernel/src/main/java/org/efaps/admin/program/bundle/BundleMaker.java
@@ -27,6 +27,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+
+import org.efaps.admin.datamodel.Type;
 import org.efaps.db.SearchQuery;
 import org.efaps.util.EFapsException;
 import org.efaps.util.cache.Cache;
@@ -42,10 +44,32 @@ import org.efaps.util.cache.CacheReloadInterface;
  */
 public final class BundleMaker {
 
-  private BundleMaker() {
-  }
+  public static UUID STATICCOMPILED =
+      UUID.fromString("76fb464e-1d14-4437-ad23-092ab12669dd");
 
-  private static Cache<StaticCompiledSource> CACHE =
+  /**
+   * Map is used to map a List of Names representing StaticSources from the
+   * eFaps-DataBase, to a Key for a Bundle
+   *
+   * @see #BUNDLES
+   */
+  private static final Map<List<String>, String> BUNDLEMAPPER =
+      new HashMap<List<String>, String>();
+
+  /**
+   * Map is used to store the relation between a Key and a Bundle
+   *
+   * @see #BUNDLEMAPPER
+   */
+  private static final Map<String, BundleInterface> BUNDLES =
+      new HashMap<String, BundleInterface>();
+
+  /**
+   * this Cache is used to store the Instances of StaticCompiledSource
+   * representing a Source from the eFaps-DataBase. It is used used to provide
+   * the most rapid access to a Name-OID relation.
+   */
+  private static final Cache<StaticCompiledSource> CACHE =
       new Cache<StaticCompiledSource>(new CacheReloadInterface() {
 
         public int priority() {
@@ -57,14 +81,27 @@ public final class BundleMaker {
         };
       });
 
-  private static Map<List<String>, String> BUNDLEMAPPER =
-      new HashMap<List<String>, String>();
+  /**
+   * a private Constructor is used to make a singelton
+   */
+  private BundleMaker() {
+  }
 
-  private static Map<String, BundleInterface> BUNDLES =
-      new HashMap<String, BundleInterface>();
-
+  /**
+   * this static method is used to get from a List of Names representing
+   * StaticSources the a key to Bundle. The method first checks if the a key for
+   * the List allready exist and if exists returns the key. Otherwise a new Key
+   * will be created using the method {@link #createNewKey(List, Class)}
+   *
+   * @param _names
+   *                List of Names representing StaticSources
+   * @param _bundleclass
+   *                The Class that will be instandiated in the case that the key
+   *                did not allready exist
+   * @return the Key to a Bundle
+   */
   public static String getBundleKey(final List<String> _names,
-                                     Class<?> _bundleclass) {
+                                    final Class<?> _bundleclass) {
     mergeList(_names);
     String key;
     synchronized (BUNDLEMAPPER) {
@@ -78,6 +115,13 @@ public final class BundleMaker {
     return key;
   }
 
+  /**
+   * This method removes all duplicated entries from the List from the end. <br>
+   * e.g. a List like ABAACD will lead to a list like BACD
+   *
+   * @param _names
+   *                List of Names representing StaticSources
+   */
   private static void mergeList(final List<String> _names) {
     final Set<String> compare = new HashSet<String>();
 
@@ -90,26 +134,15 @@ public final class BundleMaker {
     }
   }
 
-  private static void loadCache() {
-    try {
-      synchronized (CACHE) {
-        final SearchQuery query = new SearchQuery();
-        query.setQueryTypes("Admin_Program_StaticCompiled");
-        query.setExpandChildTypes(true);
-        query.addSelect("OID");
-        query.addSelect("Name");
-        query.execute();
-        while (query.next()) {
-          final String name = (String) query.get("Name");
-          final String oid = (String) query.get("OID");
-          CACHE.add(new StaticCompiledSource(oid, name));
-        }
-      }
-    } catch (final EFapsException e) {
-      // TODO
-    }
-  }
-
+  /**
+   * creates a new Key and instanciates the BundleInterface
+   *
+   * @param _names
+   *                List of Names representing StaticSources
+   * @param _bundleclass
+   *                The Class to be instanciated
+   * @return the Key to a Bundle
+   */
   private static String createNewKey(final List<String> _names,
                                      Class<?> _bundleclass) {
     final StringBuilder builder = new StringBuilder();
@@ -127,9 +160,10 @@ public final class BundleMaker {
     }
     final String ret = builder.toString();
     try {
-      final BundleInterface x = (BundleInterface) _bundleclass.newInstance();
-      x.setKey(ret, oids);
-      BUNDLES.put(ret, x);
+      final BundleInterface bundle =
+          (BundleInterface) _bundleclass.newInstance();
+      bundle.setKey(ret, oids);
+      BUNDLES.put(ret, bundle);
     } catch (final InstantiationException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -137,22 +171,71 @@ public final class BundleMaker {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    //
     return ret;
   }
 
+  /**
+   * does a Bundle for the Key exist
+   *
+   * @param _key
+   *                key to search for
+   * @return true if found, else false
+   */
   public static boolean containsKey(final String _key) {
     return BUNDLES.containsKey(_key);
   }
 
+  /**
+   * get a Bundle for a specific key
+   *
+   * @param _key
+   *                key to get the Bundle for
+   * @return the bundle if exist, else null
+   */
   public static BundleInterface getBundle(final String _key) {
     return BUNDLES.get(_key);
   }
 
+  /**
+   * method to load the StaticCompiledSources into the Cache
+   */
+  private static void loadCache() {
+    try {
+      synchronized (CACHE) {
+        final SearchQuery query = new SearchQuery();
+        query.setQueryTypes(Type.get(STATICCOMPILED).getName());
+        query.setExpandChildTypes(true);
+        query.addSelect("OID");
+        query.addSelect("Name");
+        query.execute();
+        while (query.next()) {
+          final String name = (String) query.get("Name");
+          final String oid = (String) query.get("OID");
+          CACHE.add(new StaticCompiledSource(oid, name));
+        }
+      }
+    } catch (final EFapsException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * This class represents one StaticCompiledSource from the eFaps-DataBase
+   *
+   * @author jmox
+   * @version $Id$
+   */
   private static class StaticCompiledSource implements CacheObjectInterface {
 
+    /**
+     * the Name for the StaticCompiledSource
+     */
     private final String name;
 
+    /**
+     * the oid for this StaticCompiledSource
+     */
     private final String oid;
 
     public StaticCompiledSource(final String _oid, final String _name) {
@@ -161,7 +244,7 @@ public final class BundleMaker {
     }
 
     public long getId() {
-      // TODO Auto-generated method stub
+      // not needed here
       return 0;
     }
 
@@ -170,7 +253,7 @@ public final class BundleMaker {
     }
 
     public UUID getUUID() {
-      // TODO Auto-generated method stub
+      // not needed here
       return null;
     }
 
