78671fe72cb7c222b63dd981d527011cb3578ea8
tmo <tmo@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2008-04-26 14:46:28 +0000

    - add methods to get directly parameter values instead using the parameters method which returns maps
    git-svn-id: https://efaps.googlecode.com/svn/trunk@1898 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel/src/main/java/org/efaps/admin/event/Parameter.java
+++ kernel/src/main/java/org/efaps/admin/event/Parameter.java
@@ -32,7 +32,6 @@ import org.efaps.db.Instance;
  *
  * @author jmo
  * @version $Id$
- *
  */
 public class Parameter  {
   /**
@@ -136,7 +135,43 @@ public class Parameter  {
     return (Map<String,String[]>) this.map.get(ParameterValues.PARAMETERS);
   }
 
-  public Set<?> entrySet() {
+  /**
+   * Evaluates with given key in the list of all parameters for given key and
+   * returns them (if found) as string array. If not found a <code>null</code>
+   * is returned.
+   *
+   * @param _key  name of parameter values which should returned
+   * @return array of parameter values for given key of <code>null</code> if
+   *         not exists
+   * @see #getParameters  to get the map of parameters
+   */
+  public String[] getParameterValues(final String _key)
+  {
+    final Map<String,String[]> params = getParameters();
+    return (params != null)
+           ? params.get(_key)
+           : null;
+  }
+
+  /**
+   * Evaluates with given key in the list of all parameters for given key and
+   * returns them (if found) as string with index 0 in the string array of the
+   * parameter values. If not found a <code>null</code> is returned.
+   *
+   * @param _key  name of parameter value which should returned
+   * @return value for given key or <code>null</code> if not exists
+   * @see #getParameterValues to get the string array for given key
+   */
+  public String getParameterValue(final String _key)
+  {
+    final String[] paramValues = getParameterValues(_key);
+    return ((paramValues != null) && (paramValues.length > 0))
+           ? paramValues[0]
+           : null;
+  }
+
+  public Set<?> entrySet()
+  {
     return this.map.entrySet();
   }
 
@@ -146,7 +181,8 @@ public class Parameter  {
    * @return string representation of this parameter instance.
    */
   @Override
-  public String toString() {
+  public String toString()
+  {
     return new ToStringBuilder(this)
                 .appendSuper(super.toString())
                 .append("map", this.map.toString())
