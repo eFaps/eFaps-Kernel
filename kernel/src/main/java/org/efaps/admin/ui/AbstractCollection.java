67b9ab396eb01b08b2b8044a651793003dd22008
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-05-19 22:17:24 +0000

    - adding field for classification * type and class
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel/trunk@2517 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel/src/main/java/org/efaps/admin/ui/AbstractCollection.java
+++ kernel/src/main/java/org/efaps/admin/ui/AbstractCollection.java
@@ -22,6 +22,7 @@ package org.efaps.admin.ui;
 
 import static org.efaps.admin.EFapsClassNames.COLLECTION;
 import static org.efaps.admin.EFapsClassNames.FIELD;
+import static org.efaps.admin.EFapsClassNames.FIELDCLASSIFICATION;
 import static org.efaps.admin.EFapsClassNames.FIELDCOMMAND;
 import static org.efaps.admin.EFapsClassNames.FIELDGROUP;
 import static org.efaps.admin.EFapsClassNames.FIELDHEADING;
@@ -36,6 +37,7 @@ import java.util.TreeMap;
 
 import org.efaps.admin.datamodel.Type;
 import org.efaps.admin.ui.field.Field;
+import org.efaps.admin.ui.field.FieldClassification;
 import org.efaps.admin.ui.field.FieldCommand;
 import org.efaps.admin.ui.field.FieldGroup;
 import org.efaps.admin.ui.field.FieldHeading;
@@ -47,244 +49,248 @@ import org.efaps.util.EFapsException;
 import org.efaps.util.cache.CacheReloadException;
 
 /**
- * @author tmo
+ * @author The eFaps Team
  * @version $Id$
- * TODO description
+ *
  */
-public abstract class AbstractCollection
-    extends AbstractUserInterfaceObject {
-  /**
-   * Instance variable for all field expressions.
-   *
-   * @see #addFieldExpr
-   * @see #getFieldExprIndex
-   * @see #getAllFieldExpr
-   */
-  private final Map<String, Integer> allFieldExpr
-                                      = new HashMap<String, Integer>();
+public abstract class AbstractCollection extends AbstractUserInterfaceObject
+{
+    /**
+     * Instance variable for all field expressions.
+     *
+     * @see #addFieldExpr
+     * @see #getFieldExprIndex
+     * @see #getAllFieldExpr
+     */
+    private final Map<String, Integer> allFieldExpr = new HashMap<String, Integer>();
 
-  /**
-   * All fields of the collection are stored sorted belonging to the id of the
-   * field in a tree map.
-   *
-   * @see #getFields
-   * @see #add(Field)
-   */
-  private final Map<Long, Field> fields = new TreeMap<Long, Field>();
+    /**
+     * All fields of the collection are stored sorted belonging to the id of the
+     * field in a tree map.
+     *
+     * @see #getFields
+     * @see #add(Field)
+     */
+    private final Map<Long, Field> fields = new TreeMap<Long, Field>();
 
-  /**
-   * Instance variable for the length of the field expression list.
-   *
-   * @see #allFieldExpr
-   */
-  private int selIndexLen = 1;
+    /**
+     * Instance variable for the length of the field expression list.
+     *
+     * @see #allFieldExpr
+     */
+    private int selIndexLen = 1;
 
-  /**
-   * Select string for the statement.
-   *
-   * @see #setSelect
-   * @see #getSelect
-   */
-  private String select = null;
+    /**
+     * Select string for the statement.
+     *
+     * @see #setSelect
+     * @see #getSelect
+     */
+    private String select = null;
 
-  /**
-   * Constructor passing on to the super constructor.
-   *
-   * @param _id       id of this collection
-   * @param _uuid     uuid of this collection
-   * @param _name     name of this collection
-   */
-  protected AbstractCollection(final long _id,
-                               final String _uuid,
-                               final String _name) {
-    super(_id, _uuid, _name);
-  }
-
-  /**
-   * Method to add a Field to this Collection.
-   *
-   * @param _field Field to add to this collection
-   */
-  public void add(final Field _field) {
-    this.fields.put(_field.getId(), _field);
-
-    if (_field.getReference() != null && _field.getReference().length() > 0) {
-      final String ref = _field.getReference();
-      int index, end = 0;
-      while ((index = ref.indexOf("$<", end)) > 0) {
-        index += 2;
-        end = ref.indexOf(">", index);
-        addFieldExpr(ref.substring(index, end));
-      }
+    /**
+     * Constructor passing on to the super constructor.
+     *
+     * @param _id id of this collection
+     * @param _uuid uuid of this collection
+     * @param _name name of this collection
+     */
+    protected AbstractCollection(final long _id, final String _uuid, final String _name)
+    {
+        super(_id, _uuid, _name);
     }
 
-  }
+    /**
+     * Method to add a Field to this Collection.
+     *
+     * @param _field Field to add to this collection
+     */
+    public void add(final Field _field)
+    {
+        this.fields.put(_field.getId(), _field);
 
-  /**
-   * Add a field expression to the select statement and the hash table of all
-   * field expressions. The method returns the index of the field expression. If
-   * the field expression is already added, the old index is returned, so a
-   * expression is only added once.
-   *
-   * @param _expr   field expression to add
-   * @return index of the field expression
-   * @see #getFieldExprIndex
-   * @see #getAllFieldExpr
-   * @see #allFieldExpr
-   */
-  protected int addFieldExpr(final String _expr) {
-    int ret = -1;
-    if (getAllFieldExpr().containsKey(_expr)) {
-      ret = getFieldExprIndex(_expr);
-    } else {
-      getAllFieldExpr().put(_expr, Integer.valueOf(getSelIndexLen()));
-      if (getSelect() == null) {
-        setSelect(_expr);
-      } else {
-        setSelect(getSelect() + "," + _expr);
-      }
-      ret = getSelIndexLen();
-      this.selIndexLen++;
-    }
-    return ret;
-  }
-
-  /**
-   * For the parameter <i>_expr</i> the index in the list of all field
-   * expressions is returned.
-   *
-   * @param _expr   expression for which the index is searched
-   * @return index of the field expression
-   * @see #addFieldExpr
-   * @see #getAllFieldExpr
-   * @see #allFieldExpr
-   */
-  public int getFieldExprIndex(final String _expr) {
-    int ret = -1;
-    if (getAllFieldExpr().containsKey(_expr)) {
-      final Integer ident = getAllFieldExpr().get(_expr);
-      ret = ident.intValue();
-    }
-    return ret;
-  }
-
-  // ///////////////////////////////////////////////////////////////////////////
-  /**
-   * The instance method reads all needed information for this user interface
-   * object.
-   *
-   * @see #readFromDB4Fields
-   * @throws CacheReloadException on error
-   */
-  @Override
-  protected void readFromDB() throws CacheReloadException {
-    super.readFromDB();
-    readFromDB4Fields();
-  }
-
-  /**
-   * Read all fields related to this collection object.
-   *
-   * @throws CacheReloadException on error
-   */
-  private void readFromDB4Fields() throws CacheReloadException {
-    try {
-      final Instance instance =
-          Instance.get(Type.get(COLLECTION), getId());
-      final SearchQuery query = new SearchQuery();
-      query.setExpand(instance, Type.get(FIELD).getName() + "\\Collection");
-      query.addSelect("Type");
-      query.addSelect("ID");
-      query.addSelect("Name");
-      query.executeWithoutAccessCheck();
-
-      while (query.next()) {
-        final long id = (Long) query.get("ID");
-        final String name = (String) query.get("Name");
-        final Field field;
-        final Type type = (Type) query.get("Type");
-        if (FIELDCOMMAND.getUuid().equals(type.getUUID())) {
-          field = new FieldCommand(id, null, name);
-        } else if (FIELDHEADING.getUuid().equals(type.getUUID())) {
-          field = new FieldHeading(id, null, name);
-        } else if (FIELDTABLE.getUuid().equals(type.getUUID())) {
-          field = new FieldTable(id, null, name);
-        } else if (FIELDGROUP.getUuid().equals(type.getUUID())) {
-          field = new FieldGroup(id, null, name);
-        } else if (FIELDSET.getUuid().equals(type.getUUID())) {
-          field = new FieldSet(id, null, name);
-        } else {
-          field = new Field(id, null, name);
+        if (_field.getReference() != null && _field.getReference().length() > 0) {
+            final String ref = _field.getReference();
+            int index, end = 0;
+            while ((index = ref.indexOf("$<", end)) > 0) {
+                index += 2;
+                end = ref.indexOf(">", index);
+                addFieldExpr(ref.substring(index, end));
+            }
         }
-        field.readFromDB();
-        add(field);
-      }
-    } catch (final EFapsException e) {
-      throw new CacheReloadException("could not read fields for '"
-          + getName()
-          + "'", e);
     }
-  }
 
-  /**
-   * The method takes values of the {@link #fields} and returnes them as
-   * {@link java.util.ArrayList}.
-   *
-   * @return the values of the {@link #fields} map instance as array list
-   * @see #fields
-   */
-  public List<Field> getFields() {
-    return new ArrayList<Field>(this.fields.values());
-  }
+    /**
+     * Add a field expression to the select statement and the hash table of all
+     * field expressions. The method returns the index of the field expression.
+     * If the field expression is already added, the old index is returned, so a
+     * expression is only added once.
+     *
+     * @param _expr field expression to add
+     * @return index of the field expression
+     * @see #getFieldExprIndex
+     * @see #getAllFieldExpr
+     * @see #allFieldExpr
+     */
+    protected int addFieldExpr(final String _expr)
+    {
+        int ret = -1;
+        if (getAllFieldExpr().containsKey(_expr)) {
+            ret = getFieldExprIndex(_expr);
+        } else {
+            getAllFieldExpr().put(_expr, Integer.valueOf(getSelIndexLen()));
+            if (getSelect() == null) {
+                setSelect(_expr);
+            } else {
+                setSelect(getSelect() + "," + _expr);
+            }
+            ret = getSelIndexLen();
+            this.selIndexLen++;
+        }
+        return ret;
+    }
 
-  /**
-   * @see #allFieldExpr
-   * @return the hashtable which holds single field expression
-   */
-  private Map<String, Integer> getAllFieldExpr() {
-    return this.allFieldExpr;
-  }
+    /**
+     * For the parameter <i>_expr</i> the index in the list of all field
+     * expressions is returned.
+     *
+     * @param _expr expression for which the index is searched
+     * @return index of the field expression
+     * @see #addFieldExpr
+     * @see #getAllFieldExpr
+     * @see #allFieldExpr
+     */
+    public int getFieldExprIndex(final String _expr)
+    {
+        int ret = -1;
+        if (getAllFieldExpr().containsKey(_expr)) {
+            final Integer ident = getAllFieldExpr().get(_expr);
+            ret = ident.intValue();
+        }
+        return ret;
+    }
 
-  /**
-   * Get the value of the attribute {@link #selIndexLen}.
-   *
-   * @return the value of the attribute {@link #selIndexLen}
-   * @see #selIndexLen
-   */
-  private int getSelIndexLen() {
-    return this.selIndexLen;
-  }
+    /**
+     * The instance method reads all needed information for this user interface
+     * object.
+     *
+     * @see #readFromDB4Fields
+     * @throws CacheReloadException on error
+     */
+    @Override
+    protected void readFromDB() throws CacheReloadException
+    {
+        super.readFromDB();
+        readFromDB4Fields();
+    }
 
-  /**
-   * This is the setter method for instance variable {@link #select}.
-   *
-   * @param _select
-   *                new value for instance variable {@link #select}
-   * @see #select
-   * @see #getSelect
-   */
-  protected void setSelect(final String _select) {
-    this.select = _select;
-  }
+    /**
+     * Read all fields related to this collection object.
+     *
+     * @throws CacheReloadException on error
+     */
+    private void readFromDB4Fields() throws CacheReloadException
+    {
+        try {
+            final Instance instance = Instance.get(Type.get(COLLECTION), getId());
+            final SearchQuery query = new SearchQuery();
+            query.setExpand(instance, Type.get(FIELD).getName() + "\\Collection");
+            query.addSelect("Type");
+            query.addSelect("ID");
+            query.addSelect("Name");
+            query.executeWithoutAccessCheck();
 
-  /**
-   * Get the value of the {@link #select} clause.
-   *
-   * @return the value of the {@link #select} clause
-   * @see #select
-   * @see #setSelect
-   */
-  public String getSelect() {
-    return this.select;
-  }
+            while (query.next()) {
+                final long id = (Long) query.get("ID");
+                final String name = (String) query.get("Name");
+                final Field field;
+                final Type type = (Type) query.get("Type");
+                if (FIELDCOMMAND.getUuid().equals(type.getUUID())) {
+                    field = new FieldCommand(id, null, name);
+                } else if (FIELDHEADING.getUuid().equals(type.getUUID())) {
+                    field = new FieldHeading(id, null, name);
+                } else if (FIELDTABLE.getUuid().equals(type.getUUID())) {
+                    field = new FieldTable(id, null, name);
+                } else if (FIELDGROUP.getUuid().equals(type.getUUID())) {
+                    field = new FieldGroup(id, null, name);
+                } else if (FIELDSET.getUuid().equals(type.getUUID())) {
+                    field = new FieldSet(id, null, name);
+                } else if (FIELDCLASSIFICATION.getUuid().equals(type.getUUID())) {
+                    field = new FieldClassification(id, null, name);
+                } else {
+                    field = new Field(id, null, name);
+                }
+                field.readFromDB();
+                add(field);
+            }
+        } catch (final EFapsException e) {
+            throw new CacheReloadException("could not read fields for '" + getName() + "'", e);
+        }
+    }
 
-  /**
-   * Method to get the whole map of fields.
-   *
-   * @return  Map
-   */
-  public Map<Long, Field> getFieldsMap() {
-    return this.fields;
-  }
+    /**
+     * The method takes values of the {@link #fields} and returnes them as
+     * {@link java.util.ArrayList}.
+     *
+     * @return the values of the {@link #fields} map instance as array list
+     * @see #fields
+     */
+    public List<Field> getFields()
+    {
+        return new ArrayList<Field>(this.fields.values());
+    }
 
+    /**
+     * @see #allFieldExpr
+     * @return the hashtable which holds single field expression
+     */
+    private Map<String, Integer> getAllFieldExpr()
+    {
+        return this.allFieldExpr;
+    }
+
+    /**
+     * Get the value of the attribute {@link #selIndexLen}.
+     *
+     * @return the value of the attribute {@link #selIndexLen}
+     * @see #selIndexLen
+     */
+    private int getSelIndexLen()
+    {
+        return this.selIndexLen;
+    }
+
+    /**
+     * This is the setter method for instance variable {@link #select}.
+     *
+     * @param _select new value for instance variable {@link #select}
+     * @see #select
+     * @see #getSelect
+     */
+    protected void setSelect(final String _select)
+    {
+        this.select = _select;
+    }
+
+    /**
+     * Get the value of the {@link #select} clause.
+     *
+     * @return the value of the {@link #select} clause
+     * @see #select
+     * @see #setSelect
+     */
+    public String getSelect()
+    {
+        return this.select;
+    }
+
+    /**
+     * Method to get the whole map of fields.
+     *
+     * @return Map
+     */
+    public Map<Long, Field> getFieldsMap()
+    {
+        return this.fields;
+    }
 }
