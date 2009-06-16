f8f7eaea025db1eece5b1befd6f1f48aa694d80d
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-06-16 23:53:07 +0000

    - Dimension and UoM are now part of the kernell
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@2653 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- trunk/kernel/src/main/java/org/efaps/admin/datamodel/AbstractDataModelObject.java
+++ trunk/kernel/src/main/java/org/efaps/admin/datamodel/AbstractDataModelObject.java
@@ -24,36 +24,35 @@ import org.efaps.admin.AbstractAdminObject;
 import org.efaps.util.cache.CacheReloadException;
 
 /**
- * @author tmo
+ * @author The eFaps Team
  * @version $Id$
  */
 public abstract class AbstractDataModelObject extends AbstractAdminObject
 {
 
-  /**
-   * Constructor to set the id and name of the data model object.
-   *
-   * @param _id         id to set
-   * @param _uuid       universal unique identifier
-   * @param _name name  to set
-   */
-  protected AbstractDataModelObject(final long _id,
-                                    final String _uuid,
-                                    final String _name)
-  {
-    super(_id, _uuid, _name);
-  }
+    /**
+     * Constructor to set the id and name of the data model object.
+     *
+     * @param _id id to set
+     * @param _uuid universal unique identifier
+     * @param _name name to set
+     */
+    protected AbstractDataModelObject(final long _id, final String _uuid, final String _name)
+    {
+        super(_id, _uuid, _name);
+    }
 
-  /**
-   * Initialize the cache of all data model objects.
-   *
-   * @throws CacheReloadException if cache could not be initialized
-   */
-  public static void initialize() throws CacheReloadException
-  {
-    AttributeType.initialize(AbstractDataModelObject.class);
-    SQLTable.initialize(AbstractDataModelObject.class);
-    Type.initialize(AbstractDataModelObject.class);
-    Attribute.initialize(AbstractDataModelObject.class);
-  }
+    /**
+     * Initialize the cache of all data model objects.
+     *
+     * @throws CacheReloadException if cache could not be initialized
+     */
+    public static void initialize() throws CacheReloadException
+    {
+        AttributeType.initialize(AbstractDataModelObject.class);
+        SQLTable.initialize(AbstractDataModelObject.class);
+        Type.initialize(AbstractDataModelObject.class);
+        Attribute.initialize(AbstractDataModelObject.class);
+        Dimension.initialize(AbstractDataModelObject.class);
+    }
 }
