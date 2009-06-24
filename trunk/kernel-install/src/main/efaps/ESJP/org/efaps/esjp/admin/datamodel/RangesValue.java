9d6eb0066e7f44364a3cc26576e1913ee6a07995
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-06-24 18:58:25 +0000

    - through a change of the valuelist the value for a range attribute can now be an expression
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@2674 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- trunk/kernel-install/src/main/efaps/ESJP/org/efaps/esjp/admin/datamodel/RangesValue.java
+++ trunk/kernel-install/src/main/efaps/ESJP/org/efaps/esjp/admin/datamodel/RangesValue.java
@@ -20,6 +20,7 @@
 
 package org.efaps.esjp.admin.datamodel;
 
+import java.io.StringReader;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -30,6 +31,9 @@ import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.beans.ValueList;
+import org.efaps.beans.valueparser.ParseException;
+import org.efaps.beans.valueparser.ValueParser;
 import org.efaps.db.SearchQuery;
 import org.efaps.util.EFapsException;
 
@@ -38,12 +42,13 @@ import org.efaps.util.EFapsException;
  * The Class makes a query against the Database, with the "Type" from the
  * Properties of the Parameters and returns a map sorted by the values. The key
  * returned is the value, as specified by the Property "Value". The value of the
- * map is the ID of the Objects.
- * The sorting is done by using a TreeMap, that means that the Objects are
- * sorted by their natural order.
- * Both value and key are String.
+ * map is the ID of the Objects. The sorting is done by using a TreeMap, that
+ * means that the Objects are sorted by their natural order. Both value and key
+ * are String.
+ * The value can also be an expression as defiened for DBProperties.
+ * e.g. $&lt;Name&gt; - $&lt;Value&gt;
  *
- * @author jmox
+ * @author The eFaps Team
  * @version $Id$
  */
 @EFapsUUID("6e9283bb-06a7-40a5-8420-becc4cff72f5")
@@ -51,34 +56,65 @@ import org.efaps.util.EFapsException;
 public class RangesValue implements EventExecution
 {
 
-  public Return execute(final Parameter _parameter)
-      throws EFapsException
-  {
-    final Return ret = new Return();
+    /**
+     * @see org.efaps.admin.event.EventExecution#execute(org.efaps.admin.event.Parameter)
+     * @param _parameter    parameter as defined by the efaps api
+     * @return map with value and keys
+     * @throws EFapsException on error
+     */
+    public Return execute(final Parameter _parameter) throws EFapsException
+    {
+        final Return ret = new Return();
 
-    final String type =
-        (String) ((Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES))
-            .get("Type");
+        final String type = (String) ((Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES)).get("Type");
 
-    final String value =
-        (String) ((Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES))
-            .get("Value");
+        final SearchQuery query = new SearchQuery();
+        query.setQueryTypes(type);
+        query.addSelect("ID");
 
-    final SearchQuery query = new SearchQuery();
-    query.setQueryTypes(type);
-    query.addSelect("ID");
-    query.addSelect(value);
-    query.execute();
+        final String value = (String) ((Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES)).get("Value");
 
-    final Map<String, String> map = new TreeMap<String, String>();
+        ValueList list = null;
+        if (value.contains("$<")) {
+            final ValueParser parser = new ValueParser(new StringReader(value));
+            try {
+                list = parser.ExpressionString();
+            } catch (final ParseException e) {
+                throw new EFapsException(RangesValue.class.toString(), e);
+            }
+            list.makeSelect(query);
+        } else {
+            query.addSelect(value);
+        }
+        query.execute();
 
-    while (query.next()) {
-      map.put(query.get(value).toString(),query.get("ID").toString());
+        final Map<String, String> map = new TreeMap<String, String>();
+
+        while (query.next()) {
+            if (list != null) {
+                final StringBuilder buf = new StringBuilder();
+                for (final org.efaps.beans.ValueList.Token token : list.getTokens()) {
+                    switch (token.getType()) {
+                        case EXPRESSION:
+                            final Object obj = query.get(token.getValue());
+                            buf.append(obj);
+                            break;
+                        case TEXT:
+                            buf.append(token.getValue());
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                map.put(buf.toString(), query.get("ID").toString());
+            } else {
+                map.put(query.get(value).toString(), query.get("ID").toString());
+            }
+        }
+        query.close();
+
+        ret.put(ReturnValues.VALUES, map);
+
+        return ret;
     }
-    query.close();
-
-    ret.put(ReturnValues.VALUES, map);
-
-    return ret;
-  }
 }
