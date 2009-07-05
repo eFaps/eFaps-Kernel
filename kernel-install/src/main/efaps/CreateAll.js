f89d99e9049e63b9270954d3d8ddd23dc4466b06
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-07-05 01:18:47 +0000

    - remove of StringWithUoM
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel/trunk@2729 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel-install/src/main/efaps/CreateAll.js
+++ kernel-install/src/main/efaps/CreateAll.js
@@ -457,7 +457,6 @@ function _eFapsCreateDataModelTablesStep1()  {
   _eFapsCreateAttrType(stmt, null, 'c9c98b47-d5da-4665-939c-9686c82914ac', 'PolicyLink',     'org.efaps.admin.datamodel.attributetype.StringType',         'org.efaps.admin.datamodel.ui.StringUI',         null, null);
   _eFapsCreateAttrType(stmt, null, 'adb13c3d-9506-4da2-8d75-b54c76779c6c', 'MultiLineArray', 'org.efaps.admin.datamodel.attributetype.MultiLineArrayType', 'org.efaps.admin.datamodel.ui.StringUI',         null, null);
   _eFapsCreateAttrType(stmt, null, '358d1f0e-43ae-425d-a4a0-8d5bad6f40d7', 'Decimal',        'org.efaps.admin.datamodel.attributetype.DecimalType',        'org.efaps.admin.datamodel.ui.StringUI',         null, null);
-  _eFapsCreateAttrType(stmt, null, 'b093219c-1d3f-4e30-812a-041b36ecd2e2', 'StringWithUoM',  'org.efaps.admin.datamodel.attributetype.StringWithUoMType',  'org.efaps.admin.datamodel.ui.StringWithUoMUI',  null, null);
   _eFapsCreateAttrType(stmt, null, 'f1795d01-1567-4cb3-9620-b3cd4e2af932', 'IntegerWithUoM', 'org.efaps.admin.datamodel.attributetype.IntegerWithUoMType', 'org.efaps.admin.datamodel.ui.StringWithUoMUI',  null, null);
   _eFapsCreateAttrType(stmt, null, 'de1c00bc-b041-49d6-9d85-82de0a6bee0d', 'DecimalWithUoM', 'org.efaps.admin.datamodel.attributetype.DecimalWithUoMType', 'org.efaps.admin.datamodel.ui.StringWithUoMUI',  null, null);
   
