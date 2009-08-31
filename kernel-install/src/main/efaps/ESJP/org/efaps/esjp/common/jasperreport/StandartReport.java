56e34ebcb769e72ad8f9d2b9e1c2cdcf6e129289
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-08-31 23:30:53 +0000

    - adding esjps for the basic JasperReport implementation,  issue #14 solves issue #14
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel/trunk@2964 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ kernel-install/src/main/efaps/ESJP/org/efaps/esjp/common/jasperreport/StandartReport.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2003 - 2009 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.common.jasperreport;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+/**
+ * This class must be replaced for customization, therefore it is left empty.
+ * Functional description can be found in the related "<code>_Base</code>"
+ * class.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("81e78b62-e729-4d40-b379-607270b0c64f")
+@EFapsRevision("$Rev$")
+public class StandartReport extends StandartReport_Base
+{
+}
+
