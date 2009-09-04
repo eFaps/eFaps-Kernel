bda1ea4ad3252c6cd95c5be0365e74c1846f36d1
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-09-04 03:25:55 +0000

    - update of the esjp that is used to show images in the UI
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@2996 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- trunk/kernel-install/src/main/efaps/ESJP/org/efaps/esjp/common/file/ImageField.java
+++ trunk/kernel-install/src/main/efaps/ESJP/org/efaps/esjp/common/file/ImageField.java
@@ -22,6 +22,7 @@ package org.efaps.esjp.common.file;
 
 import java.util.Map;
 
+import org.efaps.admin.datamodel.ui.FieldValue;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Parameter.ParameterValues;
@@ -31,37 +32,119 @@ import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Instance;
 
 /**
- * TODO comment
+ * The ESJP could be used to show an image in a field.
+ * <br>
+ * <br>
+ * <b>Properties:</b><br>
+ * <table>
+ * <tr>
+ * <th>Name</th>
+ * <th>Value</th>
+ * <th>Default</th>
+ * <th>mandatory</th>
+ * <th>Description</th>
+ * </tr>
+ * <tr>
+ * <td>min-height</td>
+ * <td>String. e.g. 200px, 20%</td>
+ * <td>-</td>
+ * <td>no</td>
+ * <td>Value for the stylesheet parameter for minimum height. </td>
+ * </tr>
+ * <tr>
+ * <td>max-height</td>
+ * <td>String. e.g. 200px, 20%</td>
+ * <td>-</td>
+ * <td>no</td>
+ * <td>Value for the stylesheet parameter for maximum height. </td>
+ * </tr>
+ * <tr>
+ * <td>min-width</td>
+ * <td>String. e.g. 200px, 20%</td>
+ * <td>-</td>
+ * <td>no</td>
+ * <td>Value for the stylesheet parameter for minimum width. </td>
+ * </tr>
+ * <tr>
+ * <td>max-width</td>
+ * <td>String. e.g. 200px, 20%</td>
+ * <td>-</td>
+ * <td>no</td>
+ * <td>Value for the stylesheet parameter  for maximum width. </td>
+ * </tr>
+ * </table><br>
  *
- * @author jmox
+ * To use this ESJP configure a field in a form like this following example:
+ * <pre>
+ * <code>
+ * &lt;field name="image"&gt;
+ *     &lt;property name="Label"&gt;Admin_UI_File/File.Label&lt;/property&gt;
+ *     &lt;trigger name="Admin_UI_File/file"
+ *              event="UI_FIELD_VALUE"
+ *              program="org.efaps.esjp.common.file.ImageField"
+ *              method="getViewFieldValueUI"/&gt;
+ * &lt;/field&gt;
+ * </code>
+ * </pre>
+ * Exmaple setting the min-height.
+ *
+ * <pre>
+ * <code>
+ * &lt;field name="image"&gt;
+ *     &lt;property name="Label"&gt;Admin_UI_File/File.Label&lt;/property&gt;
+ *     &lt;trigger name="Admin_UI_File/file"
+ *              event="UI_FIELD_VALUE"
+ *              program="org.efaps.esjp.common.file.ImageField"
+ *              method="getViewFieldValueUI"&gt;
+ *       &lt;property name="min-height"&gt;200px&lt;/property&gt;
+ *     &lt;/trigger&gt;
+ * &lt;/field&gt;
+ * </code>
+ * </pre>
+ *
+ *
+ * @author The eFasp TEam
  * @version $Id$
  */
 @EFapsUUID("09afa252-3548-4987-870a-616186351690")
 @EFapsRevision("$Rev$")
-public class ImageField {
+public class ImageField
+{
+    /**
+     * Render a field with an image.
+     * @param _parameter parameter as defined by the eFaps esjp API
+     * @return Return containing a snipplet
+     */
+    public Return getViewFieldValueUI(final Parameter _parameter)
+    {
+        final Instance instance = _parameter.getCallInstance();
+        final FieldValue fieldValue = (FieldValue) _parameter.get(ParameterValues.UIOBJECT);
+        final Map<?, ?> properties = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
 
+        final StringBuilder ret = new StringBuilder();
+        ret.append("<img style=\"height:auto; width:auto;");
 
-public Return getViewFieldValueUI(final Parameter _parameter) {
-    final Instance instance = _parameter.getCallInstance();
-    final Map<?, ?> properties =
-                        (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
-    String height = "auto";
-    String width = "auto";
-    if (properties.get("height") != null) {
-      height = (String) properties.get("height");
+        if (properties.get("min-height") != null) {
+            ret.append("min-height:").append((String) properties.get("min-height")).append(";");
+        }
+        if (properties.get("max-height") != null) {
+            ret.append("max-height:").append((String) properties.get("max-height")).append(";");
+        }
+        if (properties.get("min-width") != null) {
+            ret.append("min-width:").append((String) properties.get("min-width")).append(";");
+        }
+        if (properties.get("max-width") != null) {
+            ret.append("max-width:").append((String) properties.get("max-width")).append(";");
+        }
+
+        ret.append("\" ")
+            .append("id=\"").append(fieldValue.getField().getName()).append(fieldValue.getField().getId()).append("\" ")
+            .append("onClick=\"window.open(").append("'/eFaps/servlet/checkout?oid=").append(instance.getOid())
+            .append("','_blank');\" ")
+            .append("src=\"/eFaps/servlet/checkout?oid=").append(instance.getOid()).append("\" ").append("/>");
+
+        final Return retVal = new Return();
+        retVal.put(ReturnValues.SNIPLETT, ret);
+        return retVal;
     }
-    if (properties.get("width") != null) {
-      width = (String) properties.get("width");
-    }
-    final StringBuilder ret = new StringBuilder();
-    ret.append("<img style=\"height:").append(height).append("; width:")
-       .append(width).append(";\" ")
-       .append("src=\"/eFaps/servlet/checkout?oid=")
-       .append(instance.getOid()).append("\" ")
-       .append("/>");
-
-    final Return retVal = new Return();
-    retVal.put(ReturnValues.SNIPLETT, ret);
-    return retVal;
-  }
 }
