36194d75c50fbdc303656fd4a084b94d7b2f3e37
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-10-30 05:47:54 +0000

    - BugFix: the timeout of the transaction manager is rested to the default on creation of a new current therefor it must be set explicitely every time the transaction manager is set to begin
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel/trunk@3247 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- kernel/src/main/java/org/efaps/db/Context.java
+++ kernel/src/main/java/org/efaps/db/Context.java
@@ -93,6 +93,11 @@ public final class Context implements INamingBinds
      */
     private static TransactionManager TRANSMANAG = null;
 
+    /**
+     * STore the timeout for the transaction manager.
+     */
+    private static int TRANSMANAGTIMEOUT = 0;
+
     static {
         try {
             final InitialContext initCtx = new InitialContext();
@@ -100,6 +105,7 @@ public final class Context implements INamingBinds
             Context.DBTYPE = (AbstractDatabase) envCtx.lookup(RESOURCE_DBTYPE);
             Context.DATASOURCE = (DataSource) envCtx.lookup(RESOURCE_DATASOURCE);
             Context.TRANSMANAG = (TransactionManager) envCtx.lookup(RESOURCE_TRANSMANAG);
+            Context.TRANSMANAGTIMEOUT = (Integer) envCtx.lookup(RESOURCE_TRANSMANAGTIMEOUT);
         } catch (final NamingException e) {
             e.printStackTrace();
             throw new Error(e);
@@ -873,6 +879,11 @@ public final class Context implements INamingBinds
         }
 
         try {
+            //the timeout set is reseted on creation of a new Current object in the transaction manager,
+            // so if the default must be overwritten it must be set explicitly again
+            if (Context.TRANSMANAGTIMEOUT > 0) {
+                Context.TRANSMANAG.setTransactionTimeout(Context.TRANSMANAGTIMEOUT);
+            }
             Context.TRANSMANAG.begin();
         } catch (final SystemException e) {
             throw new EFapsException(Context.class, "begin.beginSystemException", e);
