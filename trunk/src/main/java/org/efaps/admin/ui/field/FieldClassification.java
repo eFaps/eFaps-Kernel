157e452863060fabaaf1e56f4392960b817be62d
tim.moxter <tim.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-11-27 19:46:40 +0000

    - define correct SVN properties
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@3407 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- trunk/src/main/java/org/efaps/admin/ui/field/FieldClassification.java
+++ trunk/src/main/java/org/efaps/admin/ui/field/FieldClassification.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Revision:        $Rev:  $
- * Last Changed:    $Date:  $
- * Last Changed By: $Author: $
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
  */
 
 package org.efaps.admin.ui.field;
@@ -26,7 +26,7 @@ import org.efaps.util.cache.CacheReloadException;
  * TODO comment!
  *
  * @author The eFaps Team
- * @version $Id: $
+ * @version $Id$
  */
 public class FieldClassification extends Field
 {
