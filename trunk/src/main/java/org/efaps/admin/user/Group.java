38bf9b280f9b897def17ee2c40b997b0f8f1d2c7
tim.moxter <tim.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-12-06 14:09:14 +0000

    - code formatting and Javadoc
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@3475 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- trunk/src/main/java/org/efaps/admin/user/Group.java
+++ trunk/src/main/java/org/efaps/admin/user/Group.java
@@ -26,233 +26,237 @@ import java.sql.Statement;
 import java.util.Map;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.efaps.db.Context;
 import org.efaps.db.transaction.ConnectionResource;
 import org.efaps.util.EFapsException;
 import org.efaps.util.cache.Cache;
 import org.efaps.util.cache.CacheReloadException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * @author tmo
+ * Class represents the instance of a group in eFaps.
+ *
+ * @author The eFaps Team
  * @version $Id$
- * @todo description
  */
-public class Group extends AbstractUserObject {
+public final class Group
+    extends AbstractUserObject
+{
+    /**
+     * Logging instance used in this class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(Group.class);
 
-  // ///////////////////////////////////////////////////////////////////////////
-  // static variables
+    /**
+     * This is the SQL select statement to select all groups from the database.
+     */
+    private static final String SQL_SELECT = "select ID, NAME, STATUS from V_USERGROUP";
 
-  /**
-   * Logging instance used in this class.
-   */
-  private static final Logger LOG = LoggerFactory.getLogger(Group.class);
+    /**
+     * Cache for all existing groups.
+     */
+    private static GroupCache CACHE = new GroupCache();
 
-  /**
-   * This is the sql select statement to select all groups from the database.
-   */
-  private static final String SQL_SELECT =
-      "select ID, NAME, STATUS from V_USERGROUP";
+    /**
+     * Create a new group instance. The method is used from the static method
+     * {@link #initialize()} to read all groups from the database.
+     *
+     * @param _id       id of the group
+     * @param _name     name of the group
+     * @param _status   status of the group
+     */
+    private Group(final long _id,
+                  final String _name,
+                  final boolean _status)
+    {
+        super(_id, null, _name, _status);
+    }
 
-  private static GroupCache CACHE = new GroupCache();
+    /**
+     * Returns the viewable name of the group. The method {@link #getName} is
+     * used for the viewing name.
+     *
+     * @param _context  context for this request
+     * @return name of the group
+     * @see #getName
+     */
+    public String getViewableName(final Context _context)
+    {
+        return getName();
+    }
 
-  /**
-   * Create a new group instance. The method is used from the static method
-   * {@link #initialise} to read all groups from the database.
-   *
-   * @param _id
-   */
-  private Group(final long _id, final String _name, final boolean _status) {
-    super(_id, null, _name, _status);
-  }
+    /**
+     * Checks, if the given person is assigned to this group.
+     *
+     * @param _person   person to test
+     * @return <i>true</i> if the person is assigned to this group, otherwise
+     *         <i>false</i>
+     * @see Person#isAssigned(Group)
+     */
+    @Override()
+    public boolean hasChildPerson(final Person _person)
+    {
+// TODO: child groups
+        return _person.isAssigned(this);
+    }
 
-  // ///////////////////////////////////////////////////////////////////////////
-  // instance methods
+    /**
+     * Method to initialize the Cache of this CacheObjectInterface.
+     *
+     * @see #CACHE
+     */
+    public static void initialize()
+    {
+        Group.CACHE.initialize(Group.class);
+    }
 
-  /**
-   * Returns the viewable name of the group. The method {@link #getName} is used
-   * for the viewing name.
-   *
-   * @param _context
-   *                context for this request
-   * @see #getName
-   */
-  public String getViewableName(final Context _context) {
-    return getName();
-  }
+    /**
+     * Returns for given parameter <i>_id</i> the instance of class
+     * {@link Group}.
+     *
+     * @param _id   id to search in the cache
+     * @return instance of class {@link Group}
+     * @see #CACHE
+     */
+    public static Group get(final long _id)
+    {
+// TODO: rewrite to use context instance
+        return Group.CACHE.get(_id);
+    }
 
-  /**
-   * Checks, if the given person is assigned to this group.
-   *
-   * @param _person
-   *                person to test
-   * @return <i>true</i> if the person is assigned to this group, otherwise
-   *         <i>false</i>
-   * @see #persons
-   * @see #getPersons
-   */
-  @Override
-  public boolean hasChildPerson(final Person _person) {
-    // TODO: child groups
-    return _person.isAssigned(this);
-  }
+    /**
+     * Returns for given parameter <i>_name</i> the instance of class
+     * {@link Group}.
+     *
+     * @param _name     name to search in the cache
+     * @return instance of class {@link Group}
+     * @see #CACHE
+     */
+    public static Group get(final String _name)
+    {
+// TODO: rewrite to use context instance
+        return Group.CACHE.get(_name);
+    }
 
-  /**
-   * Method to initialize the Cache of this CacheObjectInterface.
-   */
-  public static void initialize() {
-    CACHE.initialize(Group.class);
-  }
-
-  /**
-   * Returns for given parameter <i>_id</i> the instance of class {@link Group}.
-   *
-   * @param _id
-   *                id to search in the cache
-   * @return instance of class {@link Group}
-   * @throws CacheReloadException
-   * @see #getCache
-   * @todo rewrite to use context instance
-   */
-  public static Group get(final long _id) throws CacheReloadException {
-    return CACHE.get(_id);
-  }
-
-  /**
-   * Returns for given parameter <i>_name</i> the instance of class
-   * {@link Group}.
-   *
-   * @param _name
-   *                name to search in the cache
-   * @return instance of class {@link Group}
-   * @throws CacheReloadException
-   * @see #getCache
-   * @todo rewrite to use context instance
-   */
-  public static Group get(final String _name) throws CacheReloadException {
-    return CACHE.get(_name);
-  }
-
-  /**
-   * Returns for given parameter <i>_jaasKey</i> the instance of class
-   * {@link Group}. The parameter <i>_jaasKey</i> is the name of the group
-   * used in the given JAAS system for the group.
-   *
-   * @param _jaasSystem
-   *                JAAS system for which the JAAS key is named
-   * @param _jaasKey
-   *                key in the foreign JAAS system for which the group is
-   *                searched
-   * @return instance of class {@link Group}, or <code>null</code> if group
-   *         is not found
-   * @see #get(long)
-   */
-  static public Group getWithJAASKey(final JAASSystem _jaasSystem,
-                                     final String _jaasKey)
-                                                           throws EFapsException {
-    long groupId = 0;
-    ConnectionResource rsrc = null;
-    try {
-      rsrc = Context.getThreadContext().getConnectionResource();
-
-      Statement stmt = null;
-
-      try {
-      final  StringBuilder cmd = new StringBuilder();
-        cmd.append("select ").append("ID ").append("from V_USERGROUPJASSKEY ")
-            .append("where JAASKEY='").append(_jaasKey).append("' ").append(
-                "and JAASSYSID=").append(_jaasSystem.getId());
-
-        stmt = rsrc.getConnection().createStatement();
-       final ResultSet resultset = stmt.executeQuery(cmd.toString());
-        if (resultset.next()) {
-          groupId = resultset.getLong(1);
-        }
-        resultset.close();
-
-      } catch (final SQLException e) {
-        LOG.error("search for group for JAAS system "
-            + "'"
-            + _jaasSystem.getName()
-            + "' "
-            + "with key '"
-            + _jaasKey
-            + "' is not possible", e);
-        // TODO: exception in properties
-        throw new EFapsException(Group.class, "getWithJAASKey.SQLException", e,
-            _jaasSystem.getName(), _jaasKey);
-      }
-      finally {
+    /**
+     * Returns for given parameter <i>_jaasKey</i> the instance of class
+     * {@link Group}. The parameter <i>_jaasKey</i> is the name of the group
+     * used in the given JAAS system for the group.
+     *
+     * @param _jaasSystem   JAAS system for which the JAAS key is named
+     * @param _jaasKey      key in the foreign JAAS system for which the group
+     *                      is searched
+     * @return instance of class {@link Group}, or <code>null</code> if group
+     *         is not found
+     * @see #get(long)
+     */
+    static public Group getWithJAASKey(final JAASSystem _jaasSystem,
+                                       final String _jaasKey)
+        throws EFapsException
+    {
+        long groupId = 0;
+        ConnectionResource rsrc = null;
         try {
-          stmt.close();
-        } catch (final SQLException e) {
+            rsrc = Context.getThreadContext().getConnectionResource();
+
+            Statement stmt = null;
+
+            try {
+                final  StringBuilder cmd = new StringBuilder()
+                    .append("select ").append("ID ").append("from V_USERGROUPJASSKEY ")
+                    .append("where JAASKEY='").append(_jaasKey).append("' ")
+                    .append("and JAASSYSID=").append(_jaasSystem.getId());
+
+                stmt = rsrc.getConnection().createStatement();
+                final ResultSet resultset = stmt.executeQuery(cmd.toString());
+                if (resultset.next()) {
+                    groupId = resultset.getLong(1);
+                }
+                resultset.close();
+            } catch (final SQLException e) {
+                Group.LOG.error("search for group for JAAS system '" + _jaasSystem.getName() + "' "
+                        + "with key '" + _jaasKey + "' is not possible", e);
+// TODO: exception in properties
+                throw new EFapsException(Group.class, "getWithJAASKey.SQLException", e,
+                                         _jaasSystem.getName(), _jaasKey);
+            } finally {
+                try {
+                    stmt.close();
+                } catch (final SQLException e) {
+                }
+            }
+            rsrc.commit();
+        } finally {
+            if ((rsrc != null) && rsrc.isOpened()) {
+                rsrc.abort();
+            }
         }
-      }
-      rsrc.commit();
-    }
-    finally {
-      if ((rsrc != null) && rsrc.isOpened()) {
-        rsrc.abort();
-      }
-    }
-    return get(groupId);
-  }
-
-  private static class GroupCache extends Cache<Group> {
-
-
-    @Override
-    protected void readCache(final Map<Long, Group> cache4Id,
-        final Map<String, Group> cache4Name, final Map<UUID, Group> cache4UUID)
-        throws CacheReloadException {
-      ConnectionResource con = null;
-      try {
-        con = Context.getThreadContext().getConnectionResource();
-
-        Statement stmt = null;
-        try {
-
-          stmt = con.getConnection().createStatement();
-
-         final ResultSet resultset = stmt.executeQuery(SQL_SELECT);
-          while (resultset.next()) {
-           final long id = resultset.getLong(1);
-           final String name = resultset.getString(2).trim();
-           final boolean status = resultset.getBoolean(3);
-            LOG.debug("read group '" + name + "' (id = " + id + ")");
-            final Group group = new Group(id, name, status);
-            cache4Id.put(group.getId(), group);
-            cache4Name.put(group.getName(), group);
-            cache4UUID.put(group.getUUID(), group);
-          }
-          resultset.close();
-
-        }
-        finally {
-          if (stmt != null) {
-            stmt.close();
-          }
-        }
-
-        con.commit();
-      } catch (final SQLException e) {
-        throw new CacheReloadException("could not read groups", e);
-      } catch (final EFapsException e) {
-        throw new CacheReloadException("could not read groups", e);
-      }
-      finally {
-        if ((con != null) && con.isOpened()) {
-          try {
-            con.abort();
-          } catch (final EFapsException e) {
-            throw new CacheReloadException("could not read groups", e);
-          }
-        }
-      }
-
+        return get(groupId);
     }
 
-  }
+    /**
+     * Class to cache all group instances.
+     */
+    private static class GroupCache
+        extends Cache<Group>
+    {
+        /**
+         * Reads the information for groups from the database and stores them
+         * in the maps for the cache.
+         *
+         * @param _cache4Id     map between id and group instance used for
+         *                      caching
+         * @param _cache4Name   map between name and group instance used for
+         *                      caching
+         * @param _cache4UUID   map between UUID and group instance used for
+         *                      caching
+         * @throws CacheReloadException if reload of the cache failed
+         */
+        @Override()
+        protected void readCache(final Map<Long, Group> _cache4Id,
+                                 final Map<String, Group> _cache4Name,
+                                 final Map<UUID, Group> _cache4UUID)
+            throws CacheReloadException
+        {
+            ConnectionResource con = null;
+            try {
+                con = Context.getThreadContext().getConnectionResource();
+
+                Statement stmt = null;
+                try {
+                    stmt = con.getConnection().createStatement();
+                    final ResultSet resultset = stmt.executeQuery(Group.SQL_SELECT);
+                    while (resultset.next()) {
+                        final long id = resultset.getLong(1);
+                        final String name = resultset.getString(2).trim();
+                        final boolean status = resultset.getBoolean(3);
+                        Group.LOG.debug("read group '" + name + "' (id = " + id + ")");
+                        final Group group = new Group(id, name, status);
+                        _cache4Id.put(group.getId(), group);
+                        _cache4Name.put(group.getName(), group);
+                        _cache4UUID.put(group.getUUID(), group);
+                    }
+                    resultset.close();
+                } finally {
+                    if (stmt != null) {
+                        stmt.close();
+                    }
+                }
+                con.commit();
+            } catch (final SQLException e) {
+                throw new CacheReloadException("could not read groups", e);
+            } catch (final EFapsException e) {
+                throw new CacheReloadException("could not read groups", e);
+            } finally  {
+                if ((con != null) && con.isOpened()) {
+                    try {
+                        con.abort();
+                    } catch (final EFapsException e) {
+                        throw new CacheReloadException("could not read groups", e);
+                    }
+                }
+            }
+        }
+    }
 }
