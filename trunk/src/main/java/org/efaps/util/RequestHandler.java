bd85dd4cd9b8bf524ba5c896f1d76fb21df6a2af
tim.moxter <tim.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-12-20 12:44:54 +0000

    - code formatting and Javadoc
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@3510 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- trunk/src/main/java/org/efaps/util/RequestHandler.java
+++ trunk/src/main/java/org/efaps/util/RequestHandler.java
@@ -24,61 +24,75 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * TODO description
+ * The request handler converts URL with macros to URL without macros and
+ * correct application paths.
  *
- * @author jmox
+ * @author The eFaps Team
  * @version $Id$
  */
-public class RequestHandler {
+public final class RequestHandler
+{
+    /**
+     * URL to the image servlet.
+     */
+    public static final String URL_IMAGE = "${ROOTURL}/servlet/image/";
 
-  /**
-   * The static map stores all replacable url macros.
-   */
-  private static Map<String, String> REPLACEABLE_MACROS = null;
+    /**
+     * URL to the static content servlet (e.g. cascade style sheets, Javascript
+     * etc.)
+     */
+    public static final String URL_STATIC = "${ROOTURL}/servlet/static/";
 
-  public static final String URL_IMAGE = "${ROOTURL}/servlet/image/";
+    /**
+     * The static map stores all replaceable URL macros.
+     */
+    private static Map<String, String> REPLACEABLE_MACROS = null;
 
-  public static final String URL_STATIC = "${ROOTURL}/servlet/static/";
-
-  /**
-   * The static method replaces all known url macros by real urls.
-   *
-   * @param _url
-   *                url with url macros
-   * @return url string with replaces url macros
-   * @see #REPLACEABLE_MACROS
-   * @see #initReplacableMacros
-   */
-  public static String replaceMacrosInUrl(String _url) {
-    String url = _url;
-    if (REPLACEABLE_MACROS != null) {
-      for (final Map.Entry<String, String> entry : REPLACEABLE_MACROS
-          .entrySet()) {
-        url = url.replaceAll(entry.getKey(), entry.getValue());
-      }
+    /**
+     * Private constructor defined so that this utility class could not be
+     * instantiated.
+     */
+    private RequestHandler()
+    {
     }
-    if (url.indexOf('?') < 0) {
-      url += "?";
+
+    /**
+     * The static method replaces all known URL macros by real URL's.
+     *
+     * @param _url  URL with URL macros
+     * @return URL string with replaces URL macros
+     * @see #REPLACEABLE_MACROS
+     * @see #initReplacableMacros(String)
+     */
+    public static String replaceMacrosInUrl(final String _url)
+    {
+        String url = _url;
+        if (RequestHandler.REPLACEABLE_MACROS != null) {
+            for (final Map.Entry<String, String> entry : RequestHandler.REPLACEABLE_MACROS.entrySet()) {
+                url = url.replaceAll(entry.getKey(), entry.getValue());
+            }
+        }
+        if (url.indexOf('?') < 0) {
+            url += "?";
+        }
+        url = url.replaceAll("//", "/");
+        return url;
     }
-    url = url.replaceAll("//", "/");
-    return url;
-  }
 
-  /**
-   * Stores the key and the value of the replaceable url macros in the map
-   * {@link #REPLACEABLE_MACROS}
-   *
-   * @param _rootUrl
-   *                root url of the application used to replace
-   * @see #replaceMacrosInUrl
-   * @see #REPLACEABLE_MACROS
-   */
-  public static void initReplacableMacros(String _rootUrl) {
-    REPLACEABLE_MACROS = new HashMap<String, String>();
-    REPLACEABLE_MACROS.put("\\$\\{SERVLETURL\\}", _rootUrl + "request");
-    REPLACEABLE_MACROS.put("\\$\\{COMMONURL\\}", _rootUrl + "common");
-    REPLACEABLE_MACROS.put("\\$\\{ROOTURL\\}", _rootUrl);
-    REPLACEABLE_MACROS.put("\\$\\{ICONURL\\}", _rootUrl + "images");
-  }
-
+    /**
+     * Stores the key and the value of the replaceable URL macros in the map
+     * {@link #REPLACEABLE_MACROS}.
+     *
+     * @param _rootUrl  root URL of the application used to replace
+     * @see #replaceMacrosInUrl(String)
+     * @see #REPLACEABLE_MACROS
+     */
+    public static void initReplacableMacros(final String _rootUrl)
+    {
+        RequestHandler.REPLACEABLE_MACROS = new HashMap<String, String>();
+        RequestHandler.REPLACEABLE_MACROS.put("\\$\\{SERVLETURL\\}", _rootUrl + "request");
+        RequestHandler.REPLACEABLE_MACROS.put("\\$\\{COMMONURL\\}", _rootUrl + "common");
+        RequestHandler.REPLACEABLE_MACROS.put("\\$\\{ROOTURL\\}", _rootUrl);
+        RequestHandler.REPLACEABLE_MACROS.put("\\$\\{ICONURL\\}", _rootUrl + "images");
+    }
 }
