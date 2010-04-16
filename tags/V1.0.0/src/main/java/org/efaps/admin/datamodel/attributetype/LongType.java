05fdb20a7846f19c07bfbbb017a4981b06d94e66
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-04-16 20:54:17 +0000

    Created tag V1.0.0 for kernel
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@4057 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ tags/V1.0.0/src/main/java/org/efaps/admin/datamodel/attributetype/LongType.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.admin.datamodel.attributetype;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.efaps.admin.datamodel.Attribute;
+import org.efaps.db.query.CachedResult;
+import org.efaps.db.wrapper.AbstractSQLInsertUpdate;
+import org.efaps.util.EFapsException;
+
+/**
+ * Implements the mapping between values in the database and {@link Long}
+ * values in eFaps.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ * TODO: description
+ */
+public class LongType
+    extends AbstractType
+{
+    /**
+     * @see org.efaps.admin.datamodel.attributetype.AbstractLinkType#update(java.lang.Object, java.sql.PreparedStatement, int)
+     * @param _object   object
+     * @param _stmt     SQL statement to update the value
+     * @param _index    index in the SQL statement to update the value
+     * @return number of indexes used in the method, if the return value is null an error should be thrown
+     * @throws SQLException on error
+     */
+    /**
+     * {@inheritDoc}
+     */
+    @Override()
+    protected void prepare(final AbstractSQLInsertUpdate<?> _insertUpdate,
+                           final Attribute _attribute,
+                           final Object... _values)
+        throws SQLException
+    {
+        checkSQLColumnSize(_attribute, 1);
+        _insertUpdate.column(_attribute.getSqlColNames().get(0), eval(_values));
+    }
+
+    protected Long eval(final Object... _values)
+    {
+        final Long ret;
+
+        if ((_values == null) || (_values.length == 0) || (_values[0] == null))  {
+            ret = null;
+        } else if ((_values[0] instanceof String) && !"".equals(_values[0])) {
+            ret = Long.parseLong((String) _values[0]);
+        } else if (_values[0] instanceof Number) {
+            ret = (((Number) _values[0]).longValue());
+        } else  {
+            ret = null;
+        }
+
+        return ret;
+    }
+
+    public Object readValue(final Attribute _attribute,
+                            final CachedResult _rs,
+                            final List<Integer> _indexes)
+    {
+        return _rs.getLong(_indexes.get(0).intValue());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object readValue(final Attribute _attribute,
+                            final List<Object> _objectList)
+    {
+        Object ret = null;
+        if (_objectList.size() < 1) {
+            ret = null;
+        } else if (_objectList.size() > 1) {
+            final List<Long> list = new ArrayList<Long>();
+            for (final Object object : _objectList) {
+                if (object instanceof Number) {
+                    ret = ((Number) object).longValue();
+                } else if (object != null) {
+                    ret = Long.parseLong(object.toString());
+                }
+                list.add((Long) (object == null ? new Long(0) : object));
+            }
+            ret = list;
+        } else {
+            final Object object = _objectList.get(0);
+            if (object instanceof Number) {
+                ret = ((Number) object).longValue();
+            } else if (object != null) {
+                ret = Long.parseLong(object.toString());
+            }
+            ret = object == null ? new Long(0) : object;
+        }
+        return ret;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString4Where(final Object _value)
+        throws EFapsException
+    {
+        String ret = "";
+        if (_value instanceof Number) {
+            ret =  ((Number) _value).toString();
+        } else if (_value instanceof String) {
+            ret = (String) _value;
+        } else if (_value != null) {
+            ret = _value.toString();
+        }
+        return ret;
+    }
+}
---
+++ tags/V1.0.0/src/main/java/org/efaps/admin/ui/Table.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.admin.ui;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+import org.efaps.admin.EFapsClassNames;
+import org.efaps.db.Context;
+
+/**
+ * @author The eFaps Team
+ * @version $Id$
+ * TODO: description
+ */
+public class Table
+    extends AbstractCollection
+    implements Cloneable
+{
+    /**
+     * The static variable defines the class name in eFaps.
+     */
+    public static EFapsClassNames EFAPS_CLASSNAME = EFapsClassNames.TABLE;
+
+    /**
+     * Stores all instances of class {@link Table}.
+     *
+     * @see #getCache()
+     */
+    private static TableCache CACHE = new TableCache();
+
+    /**
+     * This is the constructor to set the id and the name.
+     *
+     * @param _id       id of the new table
+     * @param _uuid     UUID of the new table
+     * @param _name     name of the new table
+     */
+    public Table(final Long _id,
+                 final String _uuid,
+                 final String _name)
+    {
+        super(_id, _uuid, _name);
+    }
+
+    /**
+     * The instance method returns the title of the table.
+     *
+     * @param _context    context for this request
+     * @return title of the form
+     */
+    public String getViewableName(final Context _context)
+    {
+// TODO: method really required?
+        String title = "";
+        final ResourceBundle msgs = ResourceBundle.getBundle("org.efaps.properties.AttributeRessource",
+                                                             _context.getLocale());
+        try {
+            title = msgs.getString("Table.Title." + getName());
+        } catch (final MissingResourceException e) {
+        }
+        return title;
+    }
+
+    /**
+     * Creates and returns a copy of this table object.
+     *
+     * @return cloned table
+     */
+    public Table cloneTable()
+    {
+        Table ret = null;
+        try {
+            ret = (Table) super.clone();
+        } catch (final CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    /**
+     * Returns for given parameter <i>_id</i> the instance of class
+     * {@link Table}.
+     *
+     * @param _id     id to search in the cache
+     * @return instance of class {@link Table}
+     * @see #CACHE
+     */
+    public static Table get(final long _id)
+    {
+        return Table.CACHE.get(_id);
+    }
+
+    /**
+     * Returns for given parameter <i>_name</i> the instance of class
+     * {@link Table}.
+     *
+     * @param _name   name to search in the cache
+     * @return instance of class {@link Table}
+     * @see #CACHE
+     */
+    public static Table get(final String _name)
+    {
+        return Table.CACHE.get(_name);
+    }
+
+    /**
+     * Returns for given parameter <i>UUID</i> the instance of class
+     * {@link Table}.
+     *
+     * @param _uuid   UUID to search in the cache
+     * @return instance of class {@link Table}
+     * @see #CACHE
+     */
+    public static Table get(final UUID _uuid)
+    {
+        return Table.CACHE.get(_uuid);
+    }
+
+    /**
+     * Static getter method for the table {@link #CACHE}.
+     *
+     * @return value of static variable {@link #CACHE}
+     */
+    protected static UserInterfaceObjectCache<Table> getCache()
+    {
+        return Table.CACHE;
+    }
+
+    private static class TableCache
+        extends UserInterfaceObjectCache<Table>
+    {
+        protected TableCache()
+        {
+            super(Table.class);
+        }
+    }
+}
