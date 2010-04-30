45fb4fc3f43a6dbec76b27ab6f122ace22a435a5
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-04-30 00:19:20 +0000

    - Created tag V1.0.2 for kernel
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@4307 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ tags/V1.0.2/src/main/java/org/efaps/admin/datamodel/ui/StringWithUoMUI.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.admin.datamodel.ui;
+
+import org.efaps.admin.datamodel.Attribute;
+import org.efaps.admin.datamodel.Dimension;
+import org.efaps.admin.datamodel.Dimension.UoM;
+import org.efaps.admin.dbproperty.DBProperties;
+import org.efaps.admin.ui.AbstractUserInterfaceObject.TargetMode;
+import org.efaps.admin.ui.field.Field;
+import org.efaps.util.EFapsException;
+
+/**
+ * Class to represent a String for the user interface.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ *
+ */
+public class StringWithUoMUI extends AbstractUI
+{
+
+    /**
+     * Needed for serialization.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Method to get the Value for viewing in an html document.
+     *
+     * @param _fieldValue Fieldvalue the representation is requested
+     * @param _mode target mode
+     * @return value for field
+     *
+     */
+    @Override
+    public String getReadOnlyHtml(final FieldValue _fieldValue, final TargetMode _mode)
+    {
+        final StringBuilder ret = new StringBuilder();
+        final Field field = _fieldValue.getField();
+        final Object value = _fieldValue.getValue();
+
+        if (value instanceof Object[]) {
+            final Object[] values =  (Object[]) value;
+            final String tmp = values[0].toString();
+            final UoM uom = (UoM) values[1];
+            ret.append("<span name=\"").append(field.getName()).append("\" ").append(EFAPSTMPTAG).append(">")
+                   .append(tmp.replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\\n", "<br/>"))
+                   .append("</span>&nbsp;")
+                   .append("<span name=\"").append(field.getName()).append("UoM\" ").append(">")
+                   .append(uom.getName()).append("</span>");
+        }
+        return ret.toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getEditHtml(final FieldValue _fieldValue, final TargetMode _mode) throws EFapsException
+    {
+        final StringBuilder ret = new StringBuilder();
+        final Field field = _fieldValue.getField();
+        final Object value = _fieldValue.getValue();
+        String strValue = null;
+        UoM uomValue = null;
+        if (value instanceof Object[]) {
+            final Object[] values =  (Object[]) value;
+            strValue = values[0].toString();
+            uomValue = (UoM) values[1];
+        }
+
+        if (_mode.equals(TargetMode.SEARCH)) {
+            ret.append("<input type=\"text\"").append(" size=\"").append(field.getCols()).append("\" name=\"").append(
+                           field.getName()).append("\" value=\"").append((value != null ? value : "*")).append("\" />");
+        } else {
+            ret.append("<input type=\"text\" size=\"").append(field.getCols())
+                .append("\" name=\"").append(field.getName())
+                .append("\" value=\"").append(strValue != null ? strValue : "").append("\"")
+                .append(EFAPSTMPTAG).append("/>")
+                .append("<select name=\"").append(_fieldValue.getField().getName()).append("UoM\" size=\"1\">");
+
+            final Dimension dim = _fieldValue.getAttribute().getDimension();
+            for (final UoM uom : dim.getUoMs()) {
+                ret.append("<option value=\"").append(uom.getId());
+                if ((uomValue == null && uom.equals(dim.getBaseUoM())) || (uomValue != null && uomValue.equals(uom))) {
+                    ret.append("\" selected=\"selected");
+                }
+                ret.append("\">").append(uom.getName()).append("</option>");
+            }
+            ret.append("</select>");
+
+        }
+        return ret.toString();
+    }
+
+    /**
+     * @see org.efaps.admin.datamodel.ui.UIInterface#getStringValue(org.efaps.admin.datamodel.ui.FieldValue, org.efaps.admin.ui.AbstractUserInterfaceObject.TargetMode)
+     * @param _fieldValue Fieldvalue the representation is requested
+     * @param _mode the target mode
+     * @return "String-Value"
+     * @throws EFapsException on error
+     */
+    @Override
+    public String getStringValue(final FieldValue _fieldValue, final TargetMode _mode) throws EFapsException
+    {
+        return _fieldValue.getValue() == null ? "" : _fieldValue.getValue().toString();
+    }
+
+    /**
+     * Method to compare the values.
+     *
+     * @param _fieldValue first Value
+     * @param _fieldValue2 second Value
+     * @return 0
+     */
+    @Override
+    public int compare(final FieldValue _fieldValue, final FieldValue _fieldValue2)
+    {
+        final String value = _fieldValue.getValue().toString();
+        final String value2 = _fieldValue2.getValue().toString();
+        return value.compareTo(value2);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String validateValue(final String _value, final Attribute _attribute)
+    {
+        String ret = null;
+        if (_attribute != null) {
+            if (_value.length() > _attribute.getSize()) {
+                ret = DBProperties.getProperty(StringWithUoMUI.class.getName() + ".InvalidValue")
+                    + " " + _attribute.getSize();
+            }
+        }
+        return ret;
+    }
+}
---
+++ tags/V1.0.2/src/main/java/org/efaps/util/StringUtil.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.util;
+
+/**
+ *
+ * @author tmo
+ * @version $Id$
+ */
+public class StringUtil  {
+
+  /**
+   * Replace in <i>_text</i> the string <i>_replace</i> with <i>_value</i>.
+   *
+   * @param _original Text where to replace
+   * @param _what     old replace string
+   * @param _replace  new value string
+   */
+  public static String replace(String _original, String _what, String _replace)  {
+//System.out.println("StringUtil.replace("+_original+","+_what+","+_replace+")");
+    if (_replace!=null && _what!=null && _original!=null)  {
+      int lengthReplace = _replace.length();
+      int lengthWhat = _what.length();
+      int index = -lengthReplace;
+      while ((index=_original.indexOf(_what, index+lengthReplace))>=0)  {
+        _original = _original.substring(0, index) +
+                    _replace+
+                    _original.substring(index+lengthWhat);
+      }
+    }
+    return _original;
+  }
+}
