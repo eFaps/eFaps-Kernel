eb7de1bcdf9eb6f0f5853d1f418b1d965c127456
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-05-26 16:41:56 +0000

    - Created tag V1.1.0 for kernel, includes now Quartz
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel@4581 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ tags/V1.1.0/src/main/java/org/efaps/db/print/ClassSelectPart.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.db.print;
+
+import org.efaps.admin.datamodel.Classification;
+import org.efaps.admin.datamodel.Type;
+import org.efaps.db.wrapper.SQLSelect;
+
+/**
+ * Select Part for <code>class[CLASSIFICATIONNAME]</code>.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+public class ClassSelectPart
+    implements ISelectPart
+{
+
+    /**
+     * Classification this select part belongs to.
+     */
+    private final Classification classification;
+
+    /**
+     * @param _classification   classification
+     */
+    public ClassSelectPart(final String _classification)
+    {
+        this.classification = (Classification) Classification.get(_classification);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int join(final OneSelect _oneSelect,
+                    final SQLSelect _select,
+                    final int _relIndex)
+    {
+        Integer ret;
+        final String tableName = this.classification.getMainTable().getSqlTable();
+        final String column = this.classification.getAttribute(this.classification.getLinkAttributeName())
+                        .getSqlColNames().get(0);
+        ret = _oneSelect.getTableIndex(tableName, column, _relIndex);
+        if (ret == null) {
+            ret = _oneSelect.getNewTableIndex(tableName, column, _relIndex);
+            _select.leftJoin(tableName, ret, column, _relIndex, "ID");
+        }
+        return ret;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Type getType()
+    {
+        return this.classification;
+    }
+}
---
+++ tags/V1.1.0/src/main/java/org/efaps/jaas/ActionCallback.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2003 - 2010 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.jaas;
+
+import javax.security.auth.callback.Callback;
+
+/**
+ * The class defines with which action the call back is made. It defines e.g.
+ * the difference between a login and an import of all persons.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+public class ActionCallback
+    implements Callback
+{
+    /**
+     * Defines all possible modes of the call back.
+     */
+    public enum Mode {
+        /** A List of all persons is needed. */
+        ALL_PERSONS,
+        /** A 'normal' login is done. Name and Password must be checked. */
+        LOGIN,
+        /** Information about a person is needed. Password is not to check */
+        PERSON_INFORMATION,
+        /** The Password is set. The right to change the password must be checked */
+        SET_PASSWORD,
+        /** Mode is undefined. An exception should be thrown. */
+        UNDEFINED
+    }
+
+    /**
+     * @see #setMode(Mode)
+     * @see #getMode()
+     */
+    private Mode mode = ActionCallback.Mode.UNDEFINED;
+
+    /**
+     * This is the setter method for instance variable {@link #mode}.
+     *
+     * @param _mode new value for instance variable {@link #mode}
+     * @see #mode
+     * @see #getMode()
+     */
+    public void setMode(final Mode _mode)
+    {
+        this.mode = _mode;
+    }
+
+    /**
+     * This is the getter method for instance variable {@link #mode}.
+     *
+     * @return the value of the instance variable {@link #mode}.
+     * @see #mode
+     * @see #setMode(Mode)
+     */
+    public Mode getMode()
+    {
+        return this.mode;
+    }
+}
