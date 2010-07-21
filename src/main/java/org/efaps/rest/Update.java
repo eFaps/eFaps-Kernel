2a38c81b3301591789c4f97724aef3b93d44e428
jan.moxter <jan.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2010-07-21 00:19:32 +0000

    - the REST API inside eFaps must check for Access
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel/trunk@5027 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/java/org/efaps/rest/Update.java
+++ src/main/java/org/efaps/rest/Update.java
@@ -27,7 +27,6 @@ import java.io.InputStream;
 import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.UUID;
 import java.util.Map.Entry;
 
 import javax.ws.rs.Consumes;
@@ -35,36 +34,26 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
-import org.efaps.admin.user.Role;
-import org.efaps.db.Context;
 import org.efaps.update.FileType;
 import org.efaps.update.Install;
 import org.efaps.update.util.InstallationException;
 import org.efaps.util.EFapsException;
 import org.efaps.util.cache.CacheReloadException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.sun.jersey.multipart.BodyPart;
 import com.sun.jersey.multipart.BodyPartEntity;
 import com.sun.jersey.multipart.MultiPart;
 
 /**
- * TODO comment!
+ * Rest API to update files in eFaps.
  *
  * @author The eFaps Team
  * @version $Id$
  */
 @Path("/update")
 public class Update
+    extends AbstractRest
 {
-
-    /**
-     * Logging instance used to give logging information of this class.
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(Update.class);
-
-
     /**
      * Called on post to consume files used to update.
      *
@@ -75,11 +64,8 @@ public class Update
     public void updateFromFile(final MultiPart _multiPart)
     {
         try {
-            Update.LOG.info("===Start of Update via REST==");
-            //Admin_REST
-            if (Context.getThreadContext().getPerson().isAssigned(Role.get(
-                            UUID.fromString("2d142645-140d-46ad-af67-835161a8d732")))) {
-
+            if (hasAccess()) {
+                AbstractRest.LOG.info("===Start of Update via REST===");
                 final File temp = File.createTempFile("eFaps", ".tmp");
 
                 final File tmpfld = temp.getParentFile();
@@ -111,34 +97,47 @@ public class Update
 
                     final FileType filetype = FileType.getFileTypeByExensione(ending);
 
-                    Update.LOG.info("= Receieved: " + file.getName());
+                    AbstractRest.LOG.info("= Receieved: " + file.getName());
                     if (filetype != null) {
                         files.put(file, filetype);
                     }
                 }
 
                 for (final Entry<File, FileType> entry : files.entrySet()) {
-                    Update.LOG.info("...Updating " + entry.getKey().getName());
+                    AbstractRest.LOG.info("...Updating " + entry.getKey().getName());
                     final Install install = new Install();
                     install.addFile(entry.getKey().toURI().toURL(), entry.getValue().getType());
                     install.updateLatest();
                 }
-                Update.LOG.info("===End of Update via REST==");
+                AbstractRest.LOG.info("===End of Update via REST===");
             }
         } catch (final IOException e) {
-            Update.LOG.error("IOException", e);
+            AbstractRest.LOG.error("IOException", e);
         } catch (final InstallationException e) {
-            Update.LOG.error("InstallationException", e);
+            AbstractRest.LOG.error("InstallationException", e);
         } catch (final CacheReloadException e) {
-            Update.LOG.error("CacheReloadException", e);
+            AbstractRest.LOG.error("CacheReloadException", e);
         } catch (final EFapsException e) {
-            Update.LOG.error("EFapsException", e);
+            AbstractRest.LOG.error("EFapsException", e);
         }
     }
 
+    /**
+     * @return not implemented.
+     */
     @GET
     public String test()
     {
-        return "not implemented yet";
+        String ret = "";
+        try {
+            if (hasAccess()) {
+                ret = "not implemented yet";
+            }
+        } catch (final CacheReloadException e) {
+            AbstractRest.LOG.error("CacheReloadException", e);
+        } catch (final EFapsException e) {
+            AbstractRest.LOG.error("EFapsException", e);
+        }
+        return ret;
     }
 }
