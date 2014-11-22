95d717f72e6e15ac850774def155b13fab53e8bf
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-11-22 18:33:19 +0000

    - update of hibernate version
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-kernel/branches/jbpm6.2@14502 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/java/org/efaps/bpm/transaction/EFapsJTAPlatform.java
+++ src/main/java/org/efaps/bpm/transaction/EFapsJTAPlatform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2013 The eFaps Team
+ * Copyright 2003 - 2014 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,8 +24,7 @@ package org.efaps.bpm.transaction;
 import javax.transaction.TransactionManager;
 import javax.transaction.UserTransaction;
 
-import org.hibernate.service.jta.platform.internal.AbstractJtaPlatform;
-
+import org.hibernate.engine.transaction.jta.platform.internal.AbstractJtaPlatform;
 
 /**
  * Hibernate implementation for using JTA.
