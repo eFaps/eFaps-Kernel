c551f0ba7a5e32f59fc650f2c6fdae824f0264be
Jan Moxter <jan.moxter@innobix.com>
2015-04-08 11:38:18 -0500

    - changing to user of jersey 2.17, update of rest interface

--- src/main/java/org/efaps/rest/Update.java
+++ src/main/java/org/efaps/rest/Update.java
@@ -24,15 +24,18 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 
 import org.efaps.admin.AppConfigHandler;
 import org.efaps.update.FileType;
@@ -41,10 +44,10 @@ import org.efaps.update.Install.InstallFile;
 import org.efaps.update.util.InstallationException;
 import org.efaps.util.EFapsException;
 import org.efaps.util.cache.CacheReloadException;
-import org.glassfish.jersey.media.multipart.BodyPart;
 import org.glassfish.jersey.media.multipart.BodyPartEntity;
-import org.glassfish.jersey.media.multipart.MultiPart;
-import org.glassfish.jersey.media.multipart.MultiPartMediaTypes;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.joda.time.DateTime;
 
 /**
  * Rest API to update files in eFaps.
@@ -56,6 +59,7 @@ import org.glassfish.jersey.media.multipart.MultiPartMediaTypes;
 public class Update
     extends AbstractRest
 {
+
     /**
      * Name of the folder inside the "official" temporary folder.
      */
@@ -67,8 +71,8 @@ public class Update
      * @param _multiPart Mulitpart containing the update files
      */
     @POST
-    @Consumes(MultiPartMediaTypes.MULTIPART_MIXED)
-    public void updateFromFile(final MultiPart _multiPart)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public void updateFromFile(final FormDataMultiPart _multiPart)
     {
         try {
             if (hasAccess()) {
@@ -86,12 +90,21 @@ public class Update
                 final File dateFolder = new File(updateFolder, ((Long) new Date().getTime()).toString());
                 dateFolder.mkdirs();
 
-                final Map<File, FileType> files = new TreeMap<File, FileType>();
-                for (final BodyPart part : _multiPart.getBodyParts()) {
-                    final BodyPartEntity entity = (BodyPartEntity) part.getEntity();
+                final List<InstallFile> installFiles = new ArrayList<>();
+
+                final Iterator<FormDataBodyPart> filePartsIter = _multiPart.getFields("eFaps_File").iterator();
+                final Iterator<FormDataBodyPart> revPartsIter = _multiPart.getFields("eFaps_Revision").iterator();
+                final Iterator<FormDataBodyPart> datePartsIter = _multiPart.getFields("eFaps_Date").iterator();
+
+                while (filePartsIter.hasNext()) {
+                    final FormDataBodyPart filePart = filePartsIter.next();
+                    final FormDataBodyPart revPart = revPartsIter.next();
+                    final FormDataBodyPart datePart = datePartsIter.next();
+
+                    final BodyPartEntity entity = (BodyPartEntity) filePart.getEntity();
                     final InputStream in = entity.getInputStream();
 
-                    final File file = new File(dateFolder, part.getContentDisposition().getFileName());
+                    final File file = new File(dateFolder, filePart.getContentDisposition().getFileName());
 
                     final FileOutputStream out = new FileOutputStream(file);
                     final byte[] buf = new byte[1024];
@@ -107,16 +120,33 @@ public class Update
                     final FileType filetype = FileType.getFileTypeByExtension(ending);
 
                     AbstractRest.LOG.info("= Receieved: '{}'", file.getName());
-                    if (filetype != null) {
-                        files.put(file, filetype);
+
+                    final InstallFile installFile = new InstallFile().setName(file.getName())
+                                    .setURL(file.toURI().toURL()).setType(filetype.getType());
+                    if (revPart.getValue() != null && !revPart.getValue().isEmpty()) {
+                        installFile.setRevision(revPart.getValue());
                     }
+                    if (datePart.getValue() != null && !datePart.getValue().isEmpty()) {
+                        installFile.setDate(new DateTime(datePart.getValue()));
+                    }
+                    installFiles.add(installFile);
                 }
-                if (!files.isEmpty()) {
+                Collections.sort(installFiles, new Comparator<InstallFile>()
+                {
+
+                    @Override
+                    public int compare(final InstallFile _installFile0,
+                                       final InstallFile _installFile1)
+                    {
+                        return _installFile0.getName().compareTo(_installFile1.getName());
+                    }
+                });
+
+                if (!installFiles.isEmpty()) {
                     final Install install = new Install(true);
-                    for (final Entry<File, FileType> entry : files.entrySet()) {
-                        AbstractRest.LOG.info("...Adding to Update: '{}' ", entry.getKey().getName());
-                        install.addFile(new InstallFile().setURL(entry.getKey().toURI().toURL())
-                                        .setType(entry.getValue().getType()));
+                    for (final InstallFile installFile : installFiles) {
+                        AbstractRest.LOG.info("...Adding to Update: '{}' ", installFile.getName());
+                        install.addFile(installFile);
                     }
                     install.updateLatest(null);
                 }
